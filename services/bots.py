72ab867e29d6bf6d520041bd6529b14f9fa211e2
Thiago Ramos <thiago@codecov.io>
2019-08-10 17:01:15 -0300

    Making the correct bot be chosen so it has the oauthtoken

--- services/bots.py
+++ services/bots.py
@@ -1,6 +1,6 @@
 def get_repo_appropriate_bot(repo):
-    if repo.bot is not None:
+    if repo.bot is not None and repo.bot.oauth_token is not None:
         return repo.bot
-    if repo.owner.bot is not None:
+    if repo.owner.bot is not None and repo.owner.bot.oauth_token is not None:
         return repo.owner.bot
     return repo.owner
