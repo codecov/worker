d3e5c8a8758f8c56150710a36d5c903a4c7de962
Thiago Ramos <thiago@codecov.io>
2019-08-17 01:18:50 -0300

    Adding exception for when bot is not found

--- services/bots.py
+++ services/bots.py
@@ -1,6 +1,12 @@
+class RepositoryWithoutValidBotException(Exception):
+    pass
+
+
 def get_repo_appropriate_bot(repo):
     if repo.bot is not None and repo.bot.oauth_token is not None:
         return repo.bot
     if repo.owner.bot is not None and repo.owner.bot.oauth_token is not None:
         return repo.owner.bot
-    return repo.owner
+    if repo.owner.oauth_token is not None:
+        return repo.owner
+    raise RepositoryWithoutValidBotException()
