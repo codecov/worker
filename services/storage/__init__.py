07707c24efe3ce8080994ab9dc6a457a05076c9d
Thiago Ramos <thiago@codecov.io>
2019-08-21 11:39:33 -0300

    Adding choice to pick gcp as the storage method

--- services/storage/__init__.py
+++ services/storage/__init__.py
@@ -1,7 +1,13 @@
 from services.storage.minio import MinioStorageService
+from services.storage.gcp import GCPStorageService
 from helpers.config import get_config
 
 
 def get_appropriate_storage_service():
-    minio_config = get_config('services', 'minio', default={})
-    return MinioStorageService(minio_config)
+    chosen_storage = get_config('services', 'chosen_storage', 'minio')
+    if chosen_storage == 'gcp':
+        gcp_config = get_config('services', 'gcp', default={})
+        return GCPStorageService(gcp_config)
+    else:
+        minio_config = get_config('services', 'minio', default={})
+        return MinioStorageService(minio_config)
