a06b573e5058b0c6441593aa05f0e9d4be531339
Thiago Ramos <thiago@codecov.io>
2019-08-29 09:02:20 -0300

    Making more than one location be accepted as the codecov yaml

--- services/yaml/fetcher.py
+++ services/yaml/fetcher.py
@@ -1,13 +1,24 @@
+import logging
+
 import torngit
 
 from services.yaml.parser import parse_yaml_file
 
+log = logging.getLogger(__name__)
+
 
 async def fetch_commit_yaml_from_provider(commit, repository_service):
-    try:
-        location = 'codecov.yml'
-        ref = commit.commitid
-        content = await repository_service.get_source(location, ref)
-        return parse_yaml_file(content['content'])
-    except torngit.exceptions.TorngitObjectNotFoundError:
-        return None
+    possible_locations = [
+        'codecov.yml',
+        '.codecov.yml',
+        'codecov.yaml',
+        '.codecov.yaml'
+    ]
+    for location in possible_locations:
+        try:
+            ref = commit.commitid
+            content = await repository_service.get_source(location, ref)
+            return parse_yaml_file(content['content'])
+        except torngit.exceptions.TorngitObjectNotFoundError:
+            log.debug("File not in %s for commit", location)
+    return None
