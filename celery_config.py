fa10ecc54326015ccc27f089d42caf0038b2f27c
Thiago Ramos <thiago@codecov.io>
2019-10-07 15:55:51 -0300

    Separating redis instances for celery and our usage

--- celery_config.py
+++ celery_config.py
@@ -21,7 +21,7 @@ def initialize_logging(loglevel=logging.INFO, **kwargs):
     return celery_logger
 
 
-broker_url = get_config('services', 'redis_url'),
+broker_url = get_config('services', 'celery_broker') or get_config('services', 'redis_url')
 result_backend = get_config('services', 'redis_url')
 
 task_default_queue = 'new_tasks'
