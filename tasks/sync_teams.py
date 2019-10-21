e146eb70672c21c4d2a714734c77a8e166419107
Thomas Buida <thomasbuida@gmail.com>
2019-10-21 18:30:22 -0500

    Upsert teams and update organizations for owner

--- tasks/sync_teams.py
+++ tasks/sync_teams.py
@@ -1,4 +1,7 @@
 import logging
+from datetime import datetime
+from json import dumps
+from sqlalchemy.dialects.postgresql import insert
 
 from app import celery_app
 from celery_config import sync_teams_task_name
@@ -19,6 +22,9 @@ class SyncTeamsTask(BaseCodecovTask):
     """
     name = sync_teams_task_name
 
+    def write_to_db(self):
+        return True
+
     async def run_async(self, db_session, ownerid, username=None, using_integration=False, *args, **kwargs):
         owner = db_session.query(Owner).filter(
             Owner.ownerid == ownerid
@@ -27,14 +33,48 @@ class SyncTeamsTask(BaseCodecovTask):
         assert owner, 'User not found'
         service = owner.service
 
+        log.info(
+            "Sync teams",
+            extra=dict(ownerid=ownerid, username=username, using_integration=using_integration)
+        )
+
         token = self.create_token(owner, service, using_integration)
+        oauth_consumer_token = dict(key=get_config((service, 'client_id')),
+                                    secret=get_config((service, 'cliend_secret')))
 
         git = torngit.get(service,
                           token=token,
                           verify_ssl=get_verify_ssl(service),
-                          oauth_consumer_token=dict(key=get_config((service, 'client_id')),
-                                                    secret=get_config((service, 'cliend_secret'))))
+                          oauth_consumer_token=oauth_consumer_token)
 
+        # get list of teams with username, name, email, id (service_id), etc
+        teams = await git.list_teams()
+
+        updated_teams = []
+
+        for team in teams:
+            data = dict(service_id=team['id'],
+                        service=service,
+                        username=team['username'],
+                        name=team['name'],
+                        email=team['email'],
+                        avatar_url=team.get('avatar_url'),
+                        parent_service_id=team.get('parent_id'),
+                        updatestamp=datetime.now())
+
+            i = insert(Owner) \
+                     .values(data) \
+                     .on_conflict_do_update(constraint='owner_service_ids', set_=data) \
+                     .returning(Owner.ownerid)
+
+            [(ownerid,)] = db_session.execute(i).fetchall()
+            data['ownerid'] = ownerid
+            updated_teams.append(data)
+
+        team_ids = [team['ownerid'] for team in teams]
+
+        owner.updatestamp = datetime.now()
+        owner.organizations = team_ids
 
     def create_token(self, owner, service, using_integration=False):
         if using_integration:
