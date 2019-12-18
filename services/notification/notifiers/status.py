700347493f5506e40b85bc1b0c28ca17a95882ea
Thiago Ramos <thiago@codecov.io>
2019-12-18 08:52:34 -0300

    Implementing Status notifiers

---
+++ services/notification/notifiers/status.py
@@ -0,0 +1,269 @@
+import logging
+from decimal import Decimal
+
+from helpers.match import match
+from services.notification.changes import get_changes
+from services.notification.notifiers.base import AbstractBaseNotifier, Comparison
+from services.repository import get_repo_provider_service
+from services.urls import get_commit_url, get_compare_url
+from services.yaml.reader import round_number
+
+log = logging.getLogger(__name__)
+
+
+class StatusNotifier(AbstractBaseNotifier):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._repository_service = None
+
+    def is_enabled(self):
+        return True
+
+    async def build_payload(comparison):
+        raise NotImplementedError()
+
+    async def get_diff(self, comparison: Comparison):
+        repository_service = self._get_repository_service()
+        head = comparison.head.commit
+        base = comparison.base.commit
+        pull = comparison.pull
+        if self.notifier_yaml_settings.get('base') in ('auto', None, 'pr') and pull:
+            pull_diff = await repository_service.get_compare(base.commitid, head.commitid, with_commits=False)
+            return pull_diff['diff']
+        else:
+            return await repository_service.get_commit_diff(head.commitid, context=False)
+
+    def _get_repository_service(self):
+        if not self._repository_service:
+            self._repository_service = get_repo_provider_service(self.repository)
+        return self._repository_service
+
+    async def notify(self, comparison: Comparison):
+        if not self.can_we_set_this_status(comparison):
+            return {
+                'notified': False,
+                'reason': 'not_fit_criteria'
+            }
+        payload = await self.build_payload(comparison)
+        return await self.send_notification(comparison, payload)
+
+    async def status_already_exists(self, comparison, title, state, description):
+        head = comparison.head.commit
+        repository_service = self._get_repository_service()
+        statuses = await repository_service.get_commit_statuses(head.commitid)
+        if statuses:
+            exists = statuses.get(title)
+            return (
+                exists and
+                exists['state'] == state and
+                exists['description'] == description
+            )
+        return False
+
+    def get_status_external_name(self):
+        status_piece = f'/{self.title}' if self.title != 'default' else ''
+        return f'codecov/{self.context}{status_piece}'
+
+    async def send_notification(self, comparison: Comparison, payload):
+        title = self.get_status_external_name()
+        repository_service = self._get_repository_service()
+        head = comparison.head.commit
+        head_report = comparison.head.report
+        state = payload['state']
+        message = payload['message']
+        url = payload['url']
+        if not await self.status_already_exists(comparison, title, state, message):
+            state = 'success' if self.notifier_yaml_settings.get('informational') else state
+            res = await repository_service.set_commit_status(
+                commit=head.commitid,
+                status=state,
+                context=title,
+                coverage=float(head_report.totals.coverage),
+                description=message,
+                url=url
+            )
+            notification_result = {
+                'title': title,
+                'state': state,
+                'message': message,
+                'response': res
+            }
+            return {
+                'notified': True,
+                'success': True,
+                'notification_result': notification_result
+            }
+        else:
+            log.info(
+                'Status already set',
+                extra=dict(
+                    context=title,
+                    description=message,
+                    state=state
+                )
+            )
+            return {
+                'notified': False,
+                'success': None,
+                'reason': 'already_done'
+            }
+
+
+class ProjectStatusNotifier(StatusNotifier):
+
+    """Summary
+
+    Attributes:
+        context (str): The context
+
+    Possible results
+        - 100% remains the same compared to 29320f9
+        - 57.42% (+<.01%) compared to 559fe9e
+        - 85.65% (target 87%)
+        - No report found to compare against
+
+    Not implemented results (yet):
+        - Absolute coverage decreased by -{0}% but relative coverage ...
+    """
+
+    context = 'project'
+
+    def can_we_set_this_status(self, comparison):
+        head = comparison.head.commit
+        pull = comparison.pull
+        if (self.notifier_yaml_settings.get('only_pulls') or self.notifier_yaml_settings.get('base') == 'pr') and not pull:
+            return False
+        if not match(self.notifier_yaml_settings.get('branches'), head.branch):
+            return False
+        return True
+
+    def _get_project_status(self, comparison):
+        threshold = Decimal(self.notifier_yaml_settings.get('threshold') or '0.0')
+        if self.notifier_yaml_settings.get('target') not in ('auto', None):
+            head_coverage = Decimal(comparison.head.report.totals.coverage)
+            target_coverage = Decimal(self.notifier_yaml_settings.get('target').replace('%', ''))
+            state = 'success' if ((head_coverage + threshold) >= target_coverage) else 'failure'
+            head_coverage_str = round_number(self.current_yaml, head_coverage)
+            expected_coverage_str = round_number(self.current_yaml, target_coverage)
+            message = f"{head_coverage_str}% (target {expected_coverage_str}%"
+            return (state, message)
+        if comparison.base.report is None:
+            state = self.notifier_yaml_settings.get('if_not_found', 'success')
+            message = 'No report found to compare against'
+            return (state, message)
+        target_coverage = Decimal(comparison.base.report.totals.coverage)
+        head_coverage = Decimal(comparison.head.report.totals.coverage)
+        if head_coverage == target_coverage:
+            state = 'success'
+            message = f'{head_coverage}% remains the same compared to {comparison.base.commit.commitid[:7]}'
+        state = 'success' if head_coverage + threshold >= target_coverage else 'failure'
+        change_coverage = round_number(self.current_yaml, head_coverage - target_coverage)
+        message = f"{head_coverage}% (+{change_coverage}%) compared to {comparison.base.commit.commitid[:7]}"
+        return (state, message)
+
+    async def build_payload(self, comparison: Comparison):
+        commit_url = get_commit_url(comparison.head.commit)
+        pull_url = get_compare_url(comparison.base.commit, comparison.head.commit)
+        url = pull_url if comparison.pull and self.notifier_yaml_settings.get('base') in ('pr', 'auto', None) else commit_url
+        state, message = self._get_project_status(comparison)
+        return {
+            'state': state,
+            'message': message,
+            'url': url
+        }
+
+
+class PatchStatusNotifier(StatusNotifier):
+
+    context = 'patch'
+
+    async def build_payload(self, comparison: Comparison):
+        threshold = Decimal(self.notifier_yaml_settings.get('threshold') or '0.0')
+        diff = await self.get_diff(comparison)
+        totals = comparison.head.report.apply_diff(diff)
+        if self.notifier_yaml_settings.get('target') not in ('auto', None):
+            target_coverage = Decimal(self.notifier_yaml_settings.get('target').replace('%', ''))
+        else:
+            target_coverage = Decimal(comparison.base.report.totals.coverage)
+        if totals and totals.lines > 0:
+            coverage = float(totals.coverage)
+            if target_coverage is None:
+                state = self.notifier_yaml_settings.get('if_not_found', 'success')
+                message = 'No report found to compare against'
+            else:
+                state = 'success' if coverage >= target_coverage else 'failure'
+
+                if state == 'failure' and threshold is not None and coverage >= (target_coverage - threshold):
+                    state = 'success'
+                    coverage_str = round_number(self.current_yaml, coverage)
+                    threshold_str = round_number(self.current_yaml, threshold)
+                    target_str = round_number(self.current_yaml, target_coverage)
+                    message = f'{coverage_str}% of diff hit (within {threshold_str}% threshold of {target_str}%)'
+
+                else:
+                    coverage_str = round_number(self.current_yaml, coverage)
+                    target_str = round_number(self.current_yaml, target_coverage)
+                    message = f'{coverage_str}% of diff hit (target {target_str}%)'
+            return {
+                'state': state,
+                'message': message
+            }
+        if comparison.base.commit:
+            description = 'Coverage not affected when comparing {0}...{1}'.format(
+                comparison.base.commit.commitid[:7], comparison.head.commit.commitid[:7]
+            )
+        else:
+            description = 'Coverage not affected.'
+
+        return {
+            'state': 'success',
+            'message': description
+        }
+
+
+class ChangesStatusNotifier(StatusNotifier):
+
+    def is_a_change_worth_noting(self, change):
+        if not change.new and not change.deleted:
+            # has totals and not -10m => 10h
+            t = change.totals
+            if t:
+                # new missed||partial lines
+                return (t.misses + t.partials) > 0
+        return False
+
+    async def build_payload(self, comparison):
+        pull = comparison.pull
+        if self.notifier_yaml_settings.get('base') in ('auto', None, 'pr') and pull:
+            base = comparison.base.commit
+            if not base or comparison.base.report is None:
+                # must be exact hit otherwise results are wrong
+                description = 'Unable to determine changes, no report found at pull request base.'
+                state = 'success'
+                return {
+                    'state': state,
+                    'message': description
+                }
+
+        # filter changes
+        diff_json = await self.get_diff(comparison)
+        changes = get_changes(comparison.base.report, comparison.head.report, diff_json)
+        if changes:
+            changes = list(filter(self.is_a_change_worth_noting, changes))
+
+        # remove new additions
+        if changes:
+            lpc = len(changes)
+            eng = 'files have' if lpc > 1 else 'file has'
+            description = '{0} {1} unexpected coverage changes not visible in diff.'.format(lpc, eng)
+            return {
+                'state': 'success' if self.notifier_yaml_settings.get('informational') else 'failure',
+                'message': description
+            }
+
+        description = 'No unexpected coverage changes found.'
+        return {
+            'state': 'success',
+            'message': description
+        }
---
+++ services/notification/notifiers/tests/test_status.py
@@ -0,0 +1,193 @@
+import pytest
+from asyncio import Future
+
+from services.notification.notifiers.status import ProjectStatusNotifier, PatchStatusNotifier, ChangesStatusNotifier
+from torngit.status import Status
+
+
+@pytest.fixture
+def mock_repo_provider(mock_repo_provider):
+    result = Future()
+    compare_result = {
+        'diff': {
+            'files': {
+                'file_1.go': {
+                    'type': 'modified',
+                    'before': None,
+                    'segments': [
+                        {
+                            'header': ['9', '7', '9', '8'],
+                            'lines': [
+                                ' Overview',
+                                ' --------',
+                                ' ',
+                                '-Main website: `Codecov <https://codecov.io/>`_.',
+                                '+',
+                                '+website: `Codecov <https://codecov.io/>`_.',
+                                ' ',
+                                ' .. code-block:: shell-session',
+                                ' '
+                            ]
+                        },
+                        {
+                            'header': ['46', '12', '47', '19'],
+                            'lines': [
+                                ' ',
+                                ' You may need to configure a ``.coveragerc`` file. Learn more `here <http://coverage.readthedocs.org/en/latest/config.html>`_. Start with this `generic .coveragerc <https://gist.github.com/codecov-io/bf15bde2c7db1a011b6e>`_ for example.',
+                                ' ',
+                                '-We highly suggest adding `source` to your ``.coveragerc`` which solves a number of issues collecting coverage.',
+                                '+We highly suggest adding ``source`` to your ``.coveragerc``, which solves a number of issues collecting coverage.',
+                                ' ',
+                                ' .. code-block:: ini',
+                                ' ',
+                                '    [run]',
+                                '    source=your_package_name',
+                                '+   ',
+                                '+If there are multiple sources, you instead should add ``include`` to your ``.coveragerc``',
+                                '+',
+                                '+.. code-block:: ini',
+                                '+',
+                                '+   [run]',
+                                '+   include=your_package_name/*',
+                                ' ',
+                                ' unittests',
+                                ' ---------'
+                            ]
+                        },
+                        {
+                            'header': ['150', '5', '158', '4'],
+                            'lines': [
+                                ' * Twitter: `@codecov <https://twitter.com/codecov>`_.',
+                                ' * Email: `hello@codecov.io <hello@codecov.io>`_.',
+                                ' ',
+                                '-We are happy to help if you have any questions. Please contact email our Support at [support@codecov.io](mailto:support@codecov.io)',
+                                '-',
+                                '+We are happy to help if you have any questions. Please contact email our Support at `support@codecov.io <mailto:support@codecov.io>`_.'
+                            ]
+                        }
+                    ],
+                    'stats': {'added': 11, 'removed': 4}
+                }
+            }
+        },
+        'commits': [
+            {
+                'commitid': 'b92edba44fdd29fcc506317cc3ddeae1a723dd08',
+                'message': 'Update README.rst',
+                'timestamp': '2018-07-09T23:51:16Z',
+                'author': {
+                    'id': 8398772,
+                    'username': 'jerrode',
+                    'name': 'Jerrod',
+                    'email': 'jerrod@fundersclub.com'}
+            },
+            {
+                'commitid': '6ae5f1795a441884ed2847bb31154814ac01ef38',
+                'message': 'Update README.rst',
+                'timestamp': '2018-04-26T08:35:58Z',
+                'author': {
+                    'id': 11602092,
+                    'username': 'TomPed',
+                    'name': 'Thomas Pedbereznak',
+                    'email': 'tom@tomped.com'
+                }
+            }
+        ]
+    }
+    result.set_result(compare_result)
+    mock_repo_provider.get_compare.return_value = result
+    return mock_repo_provider
+
+
+class TestProjectStatusNotifier(object):
+
+    @pytest.mark.asyncio
+    async def test_build_payload(self, sample_comparison, mock_repo_provider, mock_configuration):
+        mock_configuration.params['setup']['codecov_url'] = 'test.example.br'
+        notifier = ProjectStatusNotifier(
+            repository=sample_comparison.head.commit.repository,
+            title='title',
+            notifier_yaml_settings={},
+            notifier_site_settings=True,
+            current_yaml={}
+        )
+        base_commit = sample_comparison.base.commit
+        head_commit = sample_comparison.head.commit
+        expected_result = {
+            'message': f'60.00000% (+60.00%) compared to {base_commit.commitid[:7]}',
+            'state': 'success',
+            'url': f'test.example.br/gh/{head_commit.repository.slug}/compare/{base_commit.commitid}...{head_commit.commitid}'
+        }
+        result = await notifier.build_payload(sample_comparison)
+        assert expected_result == result
+
+    @pytest.mark.asyncio
+    async def test_notify_status_doesnt_exist(self, sample_comparison, mock_repo_provider, mock_configuration):
+        statuses = Future()
+        statuses.set_result(Status([]))
+        mock_repo_provider.get_commit_statuses.return_value = statuses
+        mock_repo_provider.set_commit_status.return_value = Future()
+        mock_repo_provider.set_commit_status.return_value.set_result({'id': 'some_id'})
+        mock_configuration.params['setup']['codecov_url'] = 'test.example.br'
+        notifier = ProjectStatusNotifier(
+            repository=sample_comparison.head.commit.repository,
+            title='title',
+            notifier_yaml_settings={},
+            notifier_site_settings=True,
+            current_yaml={}
+        )
+        base_commit = sample_comparison.base.commit
+        expected_result = {
+            'notification_result': {
+                'message': f'60.00000% (+60.00%) compared to {base_commit.commitid[:7]}',
+                'response': {'id': 'some_id'},
+                'state': 'success',
+                'title': 'codecov/project/title'
+            },
+            'notified': True,
+            'success': True
+        }
+        result = await notifier.notify(sample_comparison)
+        assert expected_result == result
+
+
+class TestPatchStatusNotifier(object):
+
+    @pytest.mark.asyncio
+    async def test_build_payload(self, sample_comparison, mock_repo_provider, mock_configuration):
+        mock_configuration.params['setup']['codecov_url'] = 'test.example.br'
+        notifier = PatchStatusNotifier(
+            repository=sample_comparison.head.commit.repository,
+            title='title',
+            notifier_yaml_settings={},
+            notifier_site_settings=True,
+            current_yaml={}
+        )
+        base_commit = sample_comparison.base.commit
+        head_commit = sample_comparison.head.commit
+        expected_result = {
+            'message': f'Coverage not affected when comparing {base_commit.commitid[:7]}...{head_commit.commitid[:7]}',
+            'state': 'success'
+        }
+        result = await notifier.build_payload(sample_comparison)
+        assert expected_result == result
+
+
+class TestChangesStatusNotifier(object):
+
+    @pytest.mark.asyncio
+    async def test_build_payload(self, sample_comparison, mock_repo_provider, mock_configuration):
+        mock_configuration.params['setup']['codecov_url'] = 'test.example.br'
+        notifier = ChangesStatusNotifier(
+            repository=sample_comparison.head.commit.repository,
+            title='title',
+            notifier_yaml_settings={},
+            notifier_site_settings=True,
+            current_yaml={}
+        )
+        expected_result = {
+            'message': 'No unexpected coverage changes found.',
+            'state': 'success'
+        }
+        result = await notifier.build_payload(sample_comparison)
+        assert expected_result == result
