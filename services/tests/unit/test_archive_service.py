a352f5eaf47c16b5009ff4160abde41e94fc2ca3
Thiago Ramos <thiago@codecov.io>
2020-01-02 20:41:32 -0300

    Updating image tag name

--- services/tests/unit/test_archive_service.py
+++ services/tests/unit/test_archive_service.py
@@ -1,11 +1,14 @@
 from tests.base import BaseTestCase
 from services.archive import ArchiveService
 from database.tests.factories import RepositoryFactory
+from covreports.storage import MinioStorageService
 
 
 class TestArchiveService(BaseTestCase):
 
-    def test_read_file_hard_to_decode(self):
+    def test_read_file_hard_to_decode(self, mocker):
+        mock_read_file = mocker.patch.object(MinioStorageService, 'read_file')
+        mock_read_file.return_value = b'\x80abc'
         repo = RepositoryFactory.create()
         service = ArchiveService(repo)
         expected_result = '�abc'
