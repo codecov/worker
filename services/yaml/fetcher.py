6b60dda593481db45504fbdd9e850bf0f199a8a5
Thiago Ramos <thiago@codecov.io>
2020-03-07 23:18:47 -0300

    Allowing extra dirs where codecov.yaml can be found
    We are still dealing with the issues that refusing nested codecov.yaml files have.
    
    While we find a definitive solution, one small adjustment we can make is to accept files in certain folders that some customers are clearly using.
    
    On this case, the folders are 'dev' and '.github'

--- services/yaml/fetcher.py
+++ services/yaml/fetcher.py
@@ -1,57 +1,93 @@
 import logging
-
+from typing import Sequence, Mapping, Any
 import torngit
 
+from database.models import Commit
 from services.yaml.parser import parse_yaml_file
 
 log = logging.getLogger(__name__)
 
 
-async def fetch_commit_yaml_from_provider(commit, repository_service):
-    return await fetch_current_yaml_from_provider_via_reference(commit.commitid, repository_service)
+async def fetch_commit_yaml_from_provider(
+    commit: Commit, repository_service: torngit.base.BaseHandler
+) -> dict:
+    return await fetch_current_yaml_from_provider_via_reference(
+        commit.commitid, repository_service
+    )
 
 
-async def fetch_current_yaml_from_provider_via_reference(ref, repository_service):
-    repoid = repository_service.data['repo']['repoid']
+async def fetch_current_yaml_from_provider_via_reference(
+    ref: str, repository_service: torngit.base.BaseHandler
+) -> dict:
+    repoid = repository_service.data["repo"]["repoid"]
     location = await determine_commit_yaml_location(ref, repository_service)
     if not location:
         log.info(
             "We were not able to find the yaml on the provider API",
-            extra=dict(
-                commit=ref,
-                repoid=repoid
-            )
+            extra=dict(commit=ref, repoid=repoid),
         )
         return None
     log.info(
         "Yaml was found on provider API",
-        extra=dict(
-            commit=ref,
-            repoid=repoid,
-            location=location
-        )
+        extra=dict(commit=ref, repoid=repoid, location=location),
     )
     try:
         content = await repository_service.get_source(location, ref)
-        return parse_yaml_file(content['content'])
+        return parse_yaml_file(content["content"])
     except torngit.exceptions.TorngitObjectNotFoundError:
         log.exception(
-            "File not in %s for commit",
-            extra=dict(commit=ref, location=location)
+            "File not in %s for commit", extra=dict(commit=ref, location=location)
         )
     return None
 
 
-async def determine_commit_yaml_location(ref, repository_service):
+async def determine_commit_yaml_location(
+    ref: str, repository_service: torngit.base.BaseHandler
+) -> str:
+    """
+        Determines where in `ref` the codecov.yaml is, in a given repository
+
+        We currently look for the yaml in two different kinds of places
+            - Root level of the rpeository
+            - Specific folders that we know some customers use:
+                - `dev`
+                - `.github`
+
+    Args:
+        ref (str): The ref. Could be a branch name, tag, commit sha.
+        repository_service (torngit.base.BaseHandler): The torngit handler that can fetch this data.
+            Indirectly determines the repository
+
+    Returns:
+        str: The path of the codecov.yaml file we found. Or `None,` if not found
+    """
     possible_locations = [
-        'codecov.yml',
-        '.codecov.yml',
-        'codecov.yaml',
-        '.codecov.yaml'
+        "codecov.yml",
+        ".codecov.yml",
+        "codecov.yaml",
+        ".codecov.yaml",
     ]
+    acceptable_folders = set(["dev", ".github"])
     top_level_files = await repository_service.list_top_level_files(ref)
-    filenames = set(file_dict['path'] for file_dict in top_level_files)
-    for possibility in possible_locations:
-        if possibility in filenames:
-            return possibility
+    top_level_yaml = _search_among_files(possible_locations, top_level_files)
+    if top_level_yaml is not None:
+        return top_level_yaml
+    all_folders = set(f["name"] for f in top_level_files if f["type"] == "folder")
+    possible_folders = all_folders & acceptable_folders
+    for folder in possible_folders:
+        files_inside_folder = await repository_service.list_files(ref, folder)
+        yaml_inside_folder = _search_among_files(
+            possible_locations, files_inside_folder
+        )
+        if yaml_inside_folder:
+            return yaml_inside_folder
+    return None
+
+
+def _search_among_files(
+    desired_filenames: Sequence[str], all_files: Sequence[Mapping[str, Any]]
+) -> str:
+    for file in all_files:
+        if file["name"] in desired_filenames:
+            return file["path"]
     return None
