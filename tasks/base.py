06ea287c92a5b4611f8edeaa6f3c82e9a69f8a92
Thiago Ramos <thiago@codecov.io>
2020-04-06 01:36:47 -0300

    Revamping task-related metrics
    Starting to use the actual hooks provided by celery to measure some of the situations in celery tasks, like:
    
    - All timeouts (not only softimeouts, and including timouts that happen outside our SoftTimeLimitExceeded try except block)
    - Number of retries
    - Number of actual successes
    - Number of any exception that crashes the task (including timeouts, so there is going to be overlap with the first metric. But that makes sense semantically as a softimeout also crashes the code)

--- tasks/base.py
+++ tasks/base.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 
-from celery.exceptions import SoftTimeLimitExceeded
+from celery.worker.request import Request
 from sqlalchemy.exc import SQLAlchemyError
 
 from app import celery_app
@@ -12,14 +12,31 @@ from helpers.metrics import metrics
 log = logging.getLogger("worker")
 
 
+class BaseCodecovRequest(Request):
+
+    @property
+    def metrics_prefix(self):
+        return f"new-worker.task.{self.name}"
+
+    def on_timeout(self, soft: bool, timeout: int):
+        res = super().on_timeout(soft, timeout)
+        metrics.incr(f"{self.metrics_prefix}.timeout")
+        return res
+
+
 class BaseCodecovTask(celery_app.Task):
+    Request = BaseCodecovRequest
+
+    @property
+    def metrics_prefix(self):
+        return f"new-worker.task.{self.name}"
 
     def run(self, *args, **kwargs):
-        with metrics.timer(f"new-worker.task.{self.name}.full"):
+        with metrics.timer(f"{self.metrics_prefix}.full"):
             loop = asyncio.get_event_loop()
             db_session = get_db_session()
             try:
-                with metrics.timer(f"new-worker.task.{self.name}.run"):
+                with metrics.timer(f"{self.metrics_prefix}.run"):
                     return loop.run_until_complete(
                         self.run_async(db_session, *args, **kwargs)
                     )
@@ -30,9 +47,6 @@ class BaseCodecovTask(celery_app.Task):
                 )
                 db_session.rollback()
                 self.retry()
-            except SoftTimeLimitExceeded:
-                metrics.incr(f"new-worker.task.{self.name}.softimeout")
-                raise
             finally:
                 if self.write_to_db():
                     db_session.commit()
@@ -40,3 +54,21 @@ class BaseCodecovTask(celery_app.Task):
 
     def write_to_db(self):
         return True
+
+    def on_retry(self, *args, **kwargs):
+        res = super().on_retry(*args, **kwargs)
+        metrics.incr(f"{self.metrics_prefix}.retries")
+        return res
+
+    def on_success(self, *args, **kwargs):
+        res = super().on_success(*args, **kwargs)
+        metrics.incr(f"{self.metrics_prefix}.successes")
+        return res
+
+    def on_failure(self, *args, **kwargs):
+        """
+            Includes SoftTimeoutLimitException, for example
+        """
+        res = super().on_failure(*args, **kwargs)
+        metrics.incr(f"{self.metrics_prefix}.failures")
+        return res
