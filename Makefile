6be41df9c18cee62b6fbeb3c6de89be26e2851d4
Thiago Ramos <thiago@codecov.io>
2020-04-10 10:33:58 -0300

    Small attempt at pyinstaller
    A lot of work inspired by legacy enterprise packaging
    
    Also a lot by https://github.com/six8/pyinstaller-alpine
    
    I have no ide why it works. I stuck the hiddenimports until it stopped complaining. Nothing programatically.
    
    It feels like a house of cards in there

--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ ssh_private_key = `cat ~/.ssh/codecov-io_rsa`
 build:
 	docker build -f Dockerfile . -t codecov/worker:latest --build-arg SSH_PRIVATE_KEY="${ssh_private_key}"
 
+build.enterprise:
+	docker build -f enterprise/Dockerfile.enterprise . -t codecov/enterprise:latest
+
 test:
 	python -m pytest --cov=./
 
---
+++ enterprise/package.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# Simple wrapper around pyinstaller
+
+set -e
+set -x
+
+# Generate a random key for encryption
+random_key=$(pwgen -s 16 1)
+pyinstaller_args="${@/--random-key/--key $random_key}"
+
+# Use the hacked ldd to fix libc.musl-x86_64.so.1 location
+PATH="/pyinstaller:$PATH"
+
+hiddenimport=$(python -c "
+from glob import glob
+import celery
+
+base = celery.__file__.rsplit('/', 1)[0]
+print(
+    ' '.join(
+        [
+            '--hiddenimport celery'
+            + file.replace(base, '').replace('.py', '').replace('/', '.')
+            for file in (glob(base + '/*.py') + glob(base + '/**/*.py'))
+        ]
+    )
+)
+print(' --hiddenimport pkg_resources.py2_warn')
+print(' --hiddenimport kombu.transport.pyamqp')
+print(' --hiddenimport celery.worker.consumer')
+print(' --hiddenimport sqlalchemy.ext.baked')
+print(' --hiddenimport tasks')
+")
+
+mkdir src
+echo 'true' > src/is_enterprise
+
+# Exclude pycrypto and PyInstaller from built packages
+pyinstaller -F \
+    --exclude-module pycrypto \
+    --exclude-module PyInstaller \
+    --add-data src:/src \
+    ${hiddenimport} \
+    ${pyinstaller_args} \
+    /worker/enterprise.py
+
+# cat enterprise.spec
+
+# Clean up
+mv /worker/dist/enterprise /
+cd /
+rm -rf /home/*
+rm -rf /worker
+mv /enterprise /home
+rm -rf /pyinstaller
