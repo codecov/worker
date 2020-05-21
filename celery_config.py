cc16a975a511e6905bea5e78a96f9cc7e56a6703
Thiago Ramos <thiago@codecov.io>
2020-05-21 12:55:42 -0300

    Adding explanation that total memory is 1.5GB

--- celery_config.py
+++ celery_config.py
@@ -48,7 +48,7 @@ task_serializer = "json"
 
 accept_content = ["json"]
 
-worker_max_memory_per_child = 1500000
+worker_max_memory_per_child = 1500000  # 1.5GB
 
 # http://docs.celeryproject.org/en/latest/configuration.html?highlight=celery_redirect_stdouts#celeryd-hijack-root-logger
 worker_hijack_root_logger = False
