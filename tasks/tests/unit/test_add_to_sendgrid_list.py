486045a8934ef0bc29a024c703ef3508dc1c3f9b
Thomas Hu <tomhu1096@gmail.com>
2020-06-16 23:29:12 -0400

    log the message

--- tasks/tests/unit/test_add_to_sendgrid_list.py
+++ tasks/tests/unit/test_add_to_sendgrid_list.py
@@ -24,6 +24,7 @@ class TestAddToSendgridListTask(object):
         result = await AddToSendgridListTask().run_async(
             db_session=dbsession, ownerid=owner.ownerid, list_type="new-oauthed-users"
         )
+        print(result.__dict__)
         assert result["job_id"] == "9791f6a7-3d3b-4ae9-8f71-67bd98f33008"
 
     @pytest.mark.asyncio
