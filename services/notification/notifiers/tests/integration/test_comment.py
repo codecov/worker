ca9aa27e8babdf39aca6b4289273461d6156ab40
Thomas Buida <thomasbuida@gmail.com>
2020-06-30 08:09:19 -0500

    Fix tests for upgrade comments after copy change

--- services/notification/notifiers/tests/integration/test_comment.py
+++ services/notification/notifiers/tests/integration/test_comment.py
@@ -253,8 +253,10 @@ class TestCommentNotifierIntegration(object):
         assert result.notification_successful
         assert result.explanation is None
         expected_message = [
-            "The author of this PR, 1nf1n1t3l00p, is not an active member of this organization on Codecov.",
+            "The author of this PR, 1nf1n1t3l00p, is not an activated member of this organization on Codecov.",
             "Please [activate this user on Codecov](None/account/gh/1nf1n1t3l00p/users) to display this PR comment.",
+            "Coverage data is still being uploaded to Codecov.io for purposes of overall coverage calculations.",
+            "Please don't hesitate to email us at success@codecov.io with any questions.",
         ]
         for exp, res in zip(result.data_sent["message"], expected_message):
             assert exp == res
