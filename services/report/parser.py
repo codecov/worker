1d455e36e92e723150d191b0fa981623ee1e5bca
Thiago Ramos <thiago@codecov.io>
2020-10-06 12:49:06 -0300

    Changing how the parser works
    As we found out earlier, some of the uploaders upload the "<<<<<< EOF" section like this
    
    ```
    ...
    covered="10"/><counter type="CLASS" missed="0" covered="3"/></package><counter type="INSTRUCTION" missed="14" covered="212"/><counter type="LINE" missed="0" covered="32"/><counter type="COMPLEXITY" missed="2" covered="21"/><counter type="METHOD" missed="2" covered="21"/><counter type="CLASS" missed="0" covered="7"/></report><<<<<< EOF
    ```
    
    at the end of the line, which was not expected, and also not documented on https://docs.codecov.io/v4.4.0/reference#upload (alhtough it's a very lightly defined format there)
    
    The code that deals with that is a little too slow. So we are changing how it works. We are going to lose some flexibility over loading from files. But this is better for the medium-term.

--- services/report/parser.py
+++ services/report/parser.py
@@ -1,11 +1,14 @@
-from io import BytesIO, SEEK_END
+from io import BytesIO
 from typing import BinaryIO, List, Optional
+import string
+
+from helpers.metrics import metrics
 
 
 class ParsedUploadedReportFile(object):
     def __init__(self, filename: Optional[str], file_contents: BinaryIO):
         self.filename = filename
-        self.contents = self._strip(file_contents)
+        self.contents = file_contents.getvalue()
         self.size = len(self.contents)
 
     @property
@@ -14,40 +17,6 @@ class ParsedUploadedReportFile(object):
 
     def get_first_line(self):
         return self.file_contents.readline()
-
-    @classmethod
-    def _strip(cls, file_contents: BinaryIO) -> bytes:
-        """Strips the file in the same way that .strip() does
-
-        This should be funcitionally similar to calling file_contents.getvalue().strip
-
-        The only thing we are doing different is to try to benefit from the fact that BytesIO
-            is a mutable structure (unlike `bytes`), so we can rstrip it without having to make
-            full copies of it
-
-        On memory tests, this lead to peak memory from n * file_size to (n - 1) * file_size,
-            because rstrip would almost always have to create a copy of the original bytes. It
-            wouldn't create a full copy if the result was exactly the same as the original,
-            but those files almost always would have a trailing line break
-
-        Args:
-            file_contents (BinaryIO): The file contents we want to strip
-
-        Returns:
-            bytes: Description
-        """
-        current_index = -10
-        while True:
-            file_contents.seek(current_index, SEEK_END)
-            v = file_contents.read()
-            if v == v.rstrip():
-                # lstrips is not easily doable on BytesIO
-                # but on the other hand, it doesn't usually makes copy of the content anyway
-                # since there are not as many starting line breaks
-                return file_contents.getvalue().lstrip()
-            value_to_trim = len(v) - len(v.rstrip())
-            file_contents.seek(-value_to_trim, SEEK_END)
-            file_contents.truncate()
 
 
 class ParsedRawReport(object):
@@ -90,44 +59,71 @@ class RawReportParser(object):
     ]
 
     @classmethod
+    def _find_place_to_cut(cls, raw_report: bytes):
+        common_base = b"<<<<<<"
+        starting_point = 0
+        while 0 <= starting_point <= len(raw_report):
+            next_place = raw_report.find(common_base, starting_point)
+            if next_place >= 0:
+                starting_point = next_place + 1
+                for separator in cls.separator_lines:
+                    w = raw_report.find(
+                        separator, next_place, next_place + len(separator)
+                    )
+                    if w >= 0:
+                        yield w, separator
+                        starting_point = next_place + len(separator)
+            else:
+                return
+
+    @classmethod
+    def _get_sections_to_cut(cls, raw_report: bytes):
+        places_to_cut = sorted(cls._find_place_to_cut(raw_report))
+        if places_to_cut:
+            yield (0, places_to_cut[0][0], places_to_cut[0][1])
+            for prev, nex in zip(places_to_cut, places_to_cut[1:]):
+                yield (prev[0] + len(prev[1]), nex[0], nex[1])
+            yield (
+                places_to_cut[-1][0] + len(places_to_cut[-1][1]),
+                len(raw_report),
+                None,
+            )
+        else:
+            yield (0, len(raw_report), None)
+
+    @classmethod
+    def cut_sections(cls, raw_report: bytes):
+        whitespaces = set(string.whitespace.encode())
+        sections = cls._get_sections_to_cut(raw_report)
+        for start, end, separator in sections:
+            i_start, i_end = start, end
+            while i_start < i_end and raw_report[i_start] in whitespaces:
+                i_start += 1
+            while i_start < i_end and raw_report[i_end - 1] in whitespaces:
+                i_end -= 1
+            if i_start < i_end:
+                filename = None
+                if raw_report[i_start : i_start + len(b"# path=")] == b"# path=":
+                    content = BytesIO(raw_report)
+                    content.seek(i_start)
+                    first_line = next(iter(content))
+                    filename = first_line.split(b"# path=")[1].decode().strip()
+                    i_start = i_start + len(first_line)
+                yield {
+                    "contents": BytesIO(raw_report[i_start:i_end]),
+                    "filename": filename,
+                    "footer": separator,
+                }
+
+    @classmethod
+    @metrics.timer("services.report.parser.parse_raw_report_from_bytes")
     def parse_raw_report_from_bytes(cls, raw_report: bytes) -> ParsedRawReport:
-        return cls.parse_raw_report_from_io(BytesIO(raw_report))
+        sections = cls.cut_sections(raw_report)
+        return cls._generate_parsed_report_from_sections(sections)
 
     @classmethod
     def parse_raw_report_from_io(cls, raw_report: BinaryIO) -> ParsedRawReport:
-        sections = []
-        current_section_information = {
-            "contents": BytesIO(),
-            "filename": None,
-            "footer": None,
-        }
-        for current_line in raw_report:
-            separator_lines_found = [
-                x for x in cls.separator_lines if x in current_line
-            ]
-            if separator_lines_found:
-                separator_line_used = separator_lines_found[0]
-                remaining_content = current_line.split(separator_line_used)[0]
-                if remaining_content:
-                    current_section_information["contents"].write(remaining_content)
-                current_section_information["footer"] = separator_line_used
-                current_section_information["contents"].seek(0)
-                sections.append(current_section_information)
-                current_section_information = {
-                    "contents": BytesIO(),
-                    "filename": None,
-                    "footer": None,
-                }
-            elif current_line.startswith(b"# path="):
-                current_section_information["filename"] = (
-                    current_line.split(b"# path=")[1].decode().strip()
-                )
-            else:
-                current_section_information["contents"].write(current_line)
-        if current_section_information["contents"].tell() > 0:
-            current_section_information["contents"].seek(0)
-            sections.append(current_section_information)
-        return cls._generate_parsed_report_from_sections(sections)
+        return cls.parse_raw_report_from_bytes(raw_report.getvalue())
 
     @classmethod
     def _generate_parsed_report_from_sections(cls, sections):
