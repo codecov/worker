621141efd1c7dd614fa4aaf5a29003454a2e005a
Subhi Beidas <subhibeidas@gmail.com>
2020-11-08 17:11:22 +0200

    now storing extended celery jobs results in bakend

--- celery_config.py
+++ celery_config.py
@@ -38,7 +38,7 @@ result_backend = get_config("services", "celery_broker") or get_config(
 )
 
 result_backend_transport_options = {"visibility_timeout": 60 * 60 * 5}  # 5 hours
-
+result_extended = True
 task_default_queue = get_config(
     "setup", "tasks", "celery", "default_queue", default="celery"
 )
