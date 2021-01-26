3370d125813d8d2a2d9c83c1597c9fa264a69ef6
Eli Hooten <elir.hooten@gmail.com>
2021-01-26 14:25:00 -0600

    Bug Fixes for Various Edge Cases

--- services/activation.py
+++ services/activation.py
@@ -1,7 +1,7 @@
 import logging
 from sqlalchemy import func
 from sqlalchemy.sql import text
-from services.license import calculate_reason_for_not_being_valid, requires_license
+from services.license import get_current_license, calculate_reason_for_not_being_valid, requires_license
 
 log = logging.getLogger(__name__)
 
@@ -22,20 +22,49 @@ def activate_user(db_session, org_ownerid: int, user_ownerid: int) -> bool:
         # we will not activate if the license is invalid for any reason.
         license_status = calculate_reason_for_not_being_valid(db_session)
         if license_status is None:
-            # add user_ownerid to orgs, plan activated users.
+            # check if you have an available seat with which to activate.
             query_string = text(
                 """
-                          UPDATE owners
-                                        set plan_activated_users = array_append_unique(plan_activated_users, :user_ownerid)
-                                        where ownerid=:org_ownerid
-                                        returning ownerid, 
-                                        plan_activated_users, 
-                                        username, 
-                                        plan_activated_users @> array[:user_ownerid]::int[] as has_access;"""
+                        WITH all_plan_activated_users AS (
+                            SELECT 
+                                UNNEST(o.plan_activated_users) AS activated_owner_id
+                            FROM owners o
+                        ) SELECT count(*) as count
+                        FROM all_plan_activated_users"""
             )
-            (activation_success,) = db_session.execute(
-                query_string, {"user_ownerid": user_ownerid, "org_ownerid": org_ownerid}
-            ).fetchall()
+            seat_query = db_session.execute(query_string).fetchall()
+
+            can_activate = True
+            for result in seat_query:
+                if result[0] >= get_current_license().number_allowed_users:
+                    can_activate = False
+            # add user_ownerid to orgs, plan activated users.
+            if can_activate: 
+                query_string = text(
+                    """
+                            UPDATE owners
+                                            set plan_activated_users = array_append_unique(plan_activated_users, :user_ownerid)
+                                            where ownerid=:org_ownerid
+                                            returning ownerid, 
+                                            plan_activated_users, 
+                                            username, 
+                                            plan_activated_users @> array[:user_ownerid]::int[] as has_access;"""
+                )
+                (activation_success,) = db_session.execute(
+                    query_string, {"user_ownerid": user_ownerid, "org_ownerid": org_ownerid}
+                ).fetchall()
+            else:
+                log.info(
+                "Auto activation failed due to no seats remaining",
+                extra=dict(
+                    org_ownerid=org_ownerid,
+                    author_ownerid=user_ownerid,
+                    activation_success=False,
+                    license_status=license_status,
+                ),
+            )
+            return False
+
 
         else:
             log.info(
--- services/tests/test_activation.py
+++ services/tests/test_activation.py
@@ -154,3 +154,43 @@ class TestActivationServiceTestCase(object):
 
         was_activated = activate_user(dbsession, org.ownerid, user.ownerid)
         assert was_activated is False
+    
+    def test_pr_billing_enterprise_no_seats_for_auto_actiavation(
+        self, request, dbsession, mocker, mock_configuration, with_sql_functions
+    ):
+        mocker.patch("helpers.environment.is_enterprise", return_value=True)
+        # Create two orgs to ensure our seat availability checking works across
+        # multiple organizations.
+        # Each org consumes 5 seats of a 10 seat license.
+        org = OwnerFactory.create(
+            service="github",
+            oauth_token=None,
+            plan_activated_users=list(range(21, 24)),
+            plan_auto_activate=True,
+        )
+        dbsession.add(org)
+        dbsession.flush()
+
+        org_second = OwnerFactory.create(
+            service="github",
+            oauth_token=None,
+            plan_activated_users=list(range(25, 30)),
+            plan_auto_activate=True,
+        )
+        dbsession.add(org_second)
+        dbsession.flush()
+
+        encrypted_license = "wxWEJyYgIcFpi6nBSyKQZQeaQ9Eqpo3SXyUomAqQOzOFjdYB3A8fFM1rm+kOt2ehy9w95AzrQqrqfxi9HJIb2zLOMOB9tSy52OykVCzFtKPBNsXU/y5pQKOfV7iI3w9CHFh3tDwSwgjg8UsMXwQPOhrpvl2GdHpwEhFdaM2O3vY7iElFgZfk5D9E7qEnp+WysQwHKxDeKLI7jWCnBCBJLDjBJRSz0H7AfU55RQDqtTrnR+rsLDHOzJ80/VxwVYhb"
+        mock_configuration.params["setup"]["enterprise_license"] = encrypted_license
+        mock_configuration.params["setup"]["codecov_url"] = "https://codecov.mysite.com"
+
+        # Make a new user, this would be the 11th activated user
+        user = OwnerFactory.create_from_test_request(request)
+        dbsession.add(org_second)
+        dbsession.add(user)
+        dbsession.flush()
+
+        was_activated = activate_user(dbsession, org_second.ownerid, user.ownerid)
+        assert was_activated is False
+        dbsession.commit()
+        assert user.ownerid not in org.plan_activated_users
