5c35784b16a8ff93f315164979012d4a001cd45d
Eli Hooten <elir.hooten@gmail.com>
2021-01-26 21:13:27 -0600

    Fix Activation Test

--- services/tests/test_activation.py
+++ services/tests/test_activation.py
@@ -161,38 +161,29 @@ class TestActivationServiceTestCase(object):
         mocker.patch("helpers.environment.is_enterprise", return_value=True)
         mocker.patch("services.license._get_now", return_value=datetime(2020, 4, 2))
 
-        # Create two orgs to ensure our seat availability checking works across
-        # multiple organizations.
-        # Each org consumes 5 seats of a 10 seat license.
+        user = OwnerFactory.create_from_test_request(request)
+        dbsession.add(user)
+        dbsession.flush()
+
         org = OwnerFactory.create(
             service="github",
             oauth_token=None,
-            plan_activated_users=list(range(1, 5)),
+            plan_activated_users=[user.ownerid],
             plan_auto_activate=True,
         )
         dbsession.add(org)
         dbsession.flush()
 
-        org_second = OwnerFactory.create(
-            service="github",
-            oauth_token=None,
-            plan_activated_users=list(range(6, 10)),
-            plan_auto_activate=True,
-        )
-        dbsession.add(org_second)
-        dbsession.flush()
-
-        encrypted_license = "wxWEJyYgIcFpi6nBSyKQZQeaQ9Eqpo3SXyUomAqQOzOFjdYB3A8fFM1rm+kOt2ehy9w95AzrQqrqfxi9HJIb2zLOMOB9tSy52OykVCzFtKPBNsXU/y5pQKOfV7iI3w9CHFh3tDwSwgjg8UsMXwQPOhrpvl2GdHpwEhFdaM2O3vY7iElFgZfk5D9E7qEnp+WysQwHKxDeKLI7jWCnBCBJLDjBJRSz0H7AfU55RQDqtTrnR+rsLDHOzJ80/VxwVYhb"
+        encrypted_license = "AtFDCJPhzM0SEF6MdCay6SwaDEZjkIlxH64UAo+Qm2auVe7SsfwxvjgXviKYBK2t+mQSbQQIc9hluF4oI6r+8ZpVCYvOnHv/Qp7Ism747cGKHHGpePm/E3MDaFTGyRdTaGach9K0/3UdoGJh9Gcf1FhEiutHV2qmhWLKQFLdD9QJu31vFGChS63NH864XV3Hp62GEmhuV+/tyVNTVmh7UXShaNVEC8CU+714TUVYO0SWuysPDr6wv6mBskZE5Evb"
         mock_configuration.params["setup"]["enterprise_license"] = encrypted_license
         mock_configuration.params["setup"]["codecov_url"] = "https://codecov.mysite.com"
 
         # Make a new user, this would be the 11th activated user
-        user = OwnerFactory.create_from_test_request(request)
-        dbsession.add(org_second)
-        dbsession.add(user)
+        second_user = OwnerFactory.create_from_test_request(request)
+        dbsession.add(second_user)
         dbsession.flush()
 
-        was_activated = activate_user(dbsession, org_second.ownerid, user.ownerid)
+        was_activated = activate_user(dbsession, org.ownerid, second_user.ownerid)
         assert was_activated is False
         dbsession.commit()
-        assert user.ownerid not in org.plan_activated_users
+        assert second_user.ownerid not in org.plan_activated_users
