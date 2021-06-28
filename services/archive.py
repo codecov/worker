97e2f5ab05d2a89e5e717d0217d374012eb25ca6
Andrew Jaeger <mrjaeger00@gmail.com>
2021-06-28 14:55:23 -0400

    Use kwarg over arg

--- services/archive.py
+++ services/archive.py
@@ -194,7 +194,7 @@ class ArchiveService(object):
     def delete_repo_files(self) -> int:
         path = "v4/repos/{}".format(self.storage_hash)
         objects = self.storage.list_folder_contents(self.root, path)
-        self.storage.delete_files(self.root, [obj["name"] for obj in objects])
+        self.storage.delete_files(self.root, urls=[obj["name"] for obj in objects])
         return len(objects)
 
     """
