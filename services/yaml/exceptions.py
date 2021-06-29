5cb164066eda3c23a93870ab8ad762165bc73d52
Thiago Ramos <thiago@codecov.io>
2021-06-29 17:37:28 -0300

    Upgrade shared to 0.6.0
    Upgrade shared to 0.6.0 to include change to cerberus.
    
    Also removing an exception that was not properly removed before

--- services/yaml/exceptions.py
+++
@@ -1,4 +0,0 @@
-class InvalidYamlException(Exception):
-    def __init__(self, error_location, original_exc=None) -> None:
-        self.error_location = error_location
-        self.original_exc = original_exc
