ddd08c1f03374d783f7b0d68526de58df8745ed5
Dorian Amouroux <dor.amouroux@gmail.com>
2021-07-13 17:29:49 +0200

    add spec for compute comparison

---
+++ tasks/tests/unit/test_compute_comparison.py
@@ -0,0 +1,19 @@
+import pytest
+
+from tasks.compute_comparison import ComputeComparisonTask
+from database.tests.factories import CompareCommitFactory
+from database.enums import CompareCommitState
+
+
+class TestComputeComparisonTask(object):
+    @pytest.mark.asyncio
+    async def test_set_state_to_processed(self, dbsession):
+        comparison = CompareCommitFactory.create()
+        dbsession.add(comparison)
+        dbsession.flush()
+        task = ComputeComparisonTask()
+        await task.run_async(dbsession, comparison.id)
+        dbsession.commit()
+        dbsession.refresh(comparison)
+        dbsession.flush()
+        assert comparison.state is CompareCommitState.processed
