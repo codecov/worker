c4ae896aec9f464fec6f415523179b2609a265d1
Subhi Beidas <subhibeidas@gmail.com>
2021-08-17 16:34:28 +0300

    adding skeleton for new test

--- tasks/tests/unit/test_compute_comparison.py
+++ tasks/tests/unit/test_compute_comparison.py
@@ -31,3 +31,27 @@ class TestComputeComparisonTask(object):
             "archive", comparison.report_storage_path
         )
         assert json.loads(data_in_storage) == {"changes": [], "diff": []}
+
+
+    async def test_set_state_to_processed_non_empty report(
+        self, dbsession, mocker, mock_repo_provider, mock_storage, sample_comparison
+    ):
+        comparison = CompareCommitFactory.create()
+        dbsession.add(comparison)
+        dbsession.flush()
+        task = ComputeComparisonTask()
+        mocked_get_current_yaml = mocker.patch(
+            "tasks.compute_comparison.get_current_yaml",
+        )
+        mocked_get_current_yaml.return_value = {}
+        mocker.patch.object(
+            ReportService, "get_existing_report_for_commit", return_value=Report()
+        )
+        await task.run_async(dbsession, comparison.id)
+        dbsession.flush()
+        assert comparison.state is CompareCommitState.processed
+        data_in_storage = mock_storage.read_file(
+            "archive", comparison.report_storage_path
+        )
+        assert json.loads(data_in_storage) == {"changes": [], "diff": []}
+
