54758b8dc2f4ed16d42840a50077e815bf80d366
Thiago Ramos <thiago@codecov.io>
2021-09-06 08:58:11 -0300

    Adding proper JSON arrays processing
    We found that there is a few places where a customer might upload a json that is:
    
    1. An array
    2. Not a format supported by us
    
    And since most of our json processors expect a dict, almost all of them crash while calculating whether the json matches the type of content they parse.
    
    This changes changes the processors themselves to not crash under an array.
    
    Ideally we would also have some try except at the `process_report` function. But for now this alleviates the problem.

--- services/report/languages/rlang.py
+++ services/report/languages/rlang.py
@@ -6,7 +6,7 @@ from services.report.languages.base import BaseLanguageProcessor
 
 class RlangProcessor(BaseLanguageProcessor):
     def matches_content(self, content, first_line, name):
-        return content.get("uploader") == "R"
+        return isinstance(content, dict) and content.get("uploader") == "R"
 
     def process(
         self, name, content, path_fixer, ignored_lines, sessionid, repo_yaml=None
--- services/report/languages/rspec.py
+++ services/report/languages/rspec.py
@@ -6,7 +6,7 @@ from services.report.languages.base import BaseLanguageProcessor
 
 class RspecProcessor(BaseLanguageProcessor):
     def matches_content(self, content, first_line, name):
-        return content.get("command_name") == "RSpec"
+        return isinstance(content, dict) and content.get("command_name") == "RSpec"
 
     def process(
         self, name, content, path_fixer, ignored_lines, sessionid, repo_yaml=None
