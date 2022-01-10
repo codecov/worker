6ba5a0c81c1886798692fc0e7a0cb941c0d043a4
Thiago Ramos <thiago@codecov.io>
2022-01-10 12:13:55 -0300

    Ensuring users-basic get same treatment as users-free
    Adding users-basic to check to make it so it gets the decoration limitations from users-free

--- services/billing.py
+++ services/billing.py
@@ -12,6 +12,7 @@ class BillingPlan(Enum):
     users_monthly = "users-inappm"
     users_yearly = "users-inappy"
     users_free = "users-free"
+    users_basic = "users-basic"
     pr_monthly = "users-pr-inappm"
     pr_yearly = "users-pr-inappy"
 
@@ -22,6 +23,7 @@ def is_pr_billing_plan(plan: str) -> bool:
             BillingPlan.pr_monthly.value,
             BillingPlan.pr_yearly.value,
             BillingPlan.users_free.value,
+            BillingPlan.users_basic.value,
         ]
     else:
         license = get_current_license()
