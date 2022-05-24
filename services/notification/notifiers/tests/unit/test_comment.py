d0c9faf3327617c52169b7ff054c2f8ea04fce77
Dana <dana@codecov.io>
2022-05-24 16:04:04 +0300

    remove trailing slash

--- services/notification/notifiers/tests/unit/test_comment.py
+++ services/notification/notifiers/tests/unit/test_comment.py
@@ -3470,7 +3470,7 @@ class TestFeedbackSectionWriter(object):
         )
         res = list(await writer.write_section())
         assert res == [
-            "Help us with your feedback. Take ten seconds to tell us [how you rate us](https://about.codecov.io/nps/)."
+            "Help us with your feedback. Take ten seconds to tell us [how you rate us](https://about.codecov.io/nps)."
         ]
 
 
