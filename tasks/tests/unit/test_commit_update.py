2f3bf91c15b2acc53871ebf01ba4a3f816eeaa09
Dana <dana@codecov.io>
2022-07-27 13:44:29 +0300

    adding more tests

--- tasks/tests/unit/test_commit_update.py
+++ tasks/tests/unit/test_commit_update.py
@@ -2,6 +2,12 @@ import pytest
 
 from database.tests.factories import CommitFactory
 from tasks.commit_update import CommitUpdateTask
+from shared.torngit.exceptions import (
+    TorngitClientError,
+    TorngitRepoNotFoundError,
+    TorngitObjectNotFoundError,
+)
+from helpers.exceptions import RepositoryWithoutValidBotError
 
 
 @pytest.mark.integration
@@ -39,3 +45,140 @@ class TestCommitUpdate(object):
         assert commit.parent_commit_id is None
         assert commit.branch == "featureA"
         assert commit.pullid == 1
+
+    @pytest.mark.asyncio
+    async def test_update_commit_bot_unauthorized(
+        self,
+        mocker,
+        mock_configuration,
+        dbsession,
+        mock_redis,
+        mock_repo_provider,
+        mock_storage,
+    ):
+        mock_repo_provider.get_commit.side_effect = TorngitClientError(
+            401, "response", "message"
+        )
+        commit = CommitFactory.create(
+            message="",
+            parent_commit_id=None,
+            repository__owner__unencrypted_oauth_token="un-authorized",
+            repository__owner__username="test-acc9",
+            repository__yaml={"codecov": {"max_report_age": "764y ago"}},
+        )
+        mock_repo_provider.data = dict(
+            repo=dict(repoid=commit.repoid, commit=commit.commitid)
+        )
+        dbsession.add(commit)
+        dbsession.flush()
+
+        result = await CommitUpdateTask().run_async(
+            dbsession, commit.repoid, commit.commitid
+        )
+        assert {"was_updated": False} == result
+        assert commit.message == ""
+        assert commit.parent_commit_id is None
+
+    @pytest.mark.asyncio
+    async def test_update_commit_no_bot(
+        self,
+        mocker,
+        mock_configuration,
+        dbsession,
+        mock_redis,
+        mock_repo_provider,
+        mock_storage,
+    ):
+        mock_get_repo_service = mocker.patch(
+            "tasks.commit_update.get_repo_provider_service"
+        )
+        mock_get_repo_service.side_effect = RepositoryWithoutValidBotError()
+        commit = CommitFactory.create(
+            message="",
+            parent_commit_id=None,
+            commitid="a2d3e3c30547a000f026daa47610bb3f7b63aece",
+            repository__owner__unencrypted_oauth_token="ghp_test3c8iyfspq6h4s9ugpmq19qp7826rv20o",
+            repository__owner__username="test-acc9",
+            repository__yaml={"codecov": {"max_report_age": "764y ago"}},
+            repository__name="test_example",
+        )
+        dbsession.add(commit)
+        dbsession.flush()
+        result = await CommitUpdateTask().run_async(
+            dbsession, commit.repoid, commit.commitid
+        )
+        expected_result = {"was_updated": False}
+        assert expected_result == result
+        assert commit.message == ""
+        assert commit.parent_commit_id is None
+
+    @pytest.mark.asyncio
+    async def test_update_commit_repo_not_found(
+        self,
+        mocker,
+        mock_configuration,
+        dbsession,
+        mock_redis,
+        mock_repo_provider,
+        mock_storage,
+    ):
+        mock_get_repo_service = mocker.patch(
+            "tasks.commit_update.get_repo_provider_service"
+        )
+        mock_get_repo_service.side_effect = TorngitRepoNotFoundError(
+            "fake_response", "message"
+        )
+        commit = CommitFactory.create(
+            message="",
+            parent_commit_id=None,
+            repository__owner__unencrypted_oauth_token="ghp_test3c8iyfspq6h4s9ugpmq19qp7826rv20o",
+            repository__owner__username="test-acc9",
+            repository__yaml={"codecov": {"max_report_age": "764y ago"}},
+            repository__name="test_example",
+        )
+        dbsession.add(commit)
+        dbsession.flush()
+
+        result = await CommitUpdateTask().run_async(
+            dbsession, commit.repoid, commit.commitid
+        )
+        expected_result = {"was_updated": False}
+        assert expected_result == result
+        assert commit.message == ""
+        assert commit.parent_commit_id is None
+
+    @pytest.mark.asyncio
+    async def test_update_commit_not_found(
+        self,
+        mocker,
+        mock_configuration,
+        dbsession,
+        mock_redis,
+        mock_repo_provider,
+        mock_storage,
+    ):
+
+        mock_update_commit_from_provider = mocker.patch(
+            "tasks.commit_update.update_commit_from_provider_info"
+        )
+        mock_update_commit_from_provider.side_effect = TorngitObjectNotFoundError(
+            "fake_response", "message"
+        )
+        commit = CommitFactory.create(
+            message="",
+            parent_commit_id=None,
+            repository__owner__unencrypted_oauth_token="ghp_test3c8iyfspq6h4s9ugpmq19qp7826rv20o",
+            repository__owner__username="test-acc9",
+            repository__yaml={"codecov": {"max_report_age": "764y ago"}},
+            repository__name="test_example",
+        )
+        dbsession.add(commit)
+        dbsession.flush()
+
+        result = await CommitUpdateTask().run_async(
+            dbsession, commit.repoid, commit.commitid
+        )
+        expected_result = {"was_updated": False}
+        assert expected_result == result
+        assert commit.message == ""
+        assert commit.parent_commit_id is None
