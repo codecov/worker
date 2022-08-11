16beaa46e8b71d5607e591dbad762eeca53e0514
Scott Nelson <scott@codecov.io>
2022-08-10 22:08:44 -0400

    Remove old backfill task

--- tasks/timeseries_backfill.py
+++ tasks/timeseries_backfill.py
@@ -10,63 +10,10 @@ from app import celery_app
 from database.models import Commit, Repository
 from database.models.timeseries import Dataset
 from helpers.timeseries import backfill_batch_size, timeseries_enabled
-from services.timeseries import (
-    repository_commits_query,
-    repository_datasets_query,
-    save_commit_measurements,
-    save_repository_measurements,
-)
+from services.timeseries import repository_commits_query, save_commit_measurements
 from tasks.base import BaseCodecovTask
 
 log = logging.getLogger(__name__)
-
-
-class TimeseriesBackfillTask(BaseCodecovTask):
-
-    # TODO: add name to `shared.celery_config`
-    name = "app.tasks.timeseries.backfill"
-
-    async def run_async(
-        self,
-        db_session: Session,
-        *,
-        repoid: int,
-        start_date: str,
-        end_date: str,
-        dataset_names: Iterable[str] = None,
-        **kwargs,
-    ):
-        repository = db_session.query(Repository).filter_by(repoid=repoid).first()
-        if not repository:
-            log.error(
-                "Repository not found",
-                extra=dict(repoid=repoid),
-            )
-            return {"successful": False}
-
-        try:
-            start_date = datetime.fromisoformat(start_date)
-            end_date = datetime.fromisoformat(end_date)
-            if dataset_names is None:
-                datasets = repository_datasets_query(repository, backfilled=False)
-                dataset_names = [dataset.name for dataset in datasets]
-            save_repository_measurements(
-                repository,
-                start_date,
-                end_date,
-                dataset_names=dataset_names,
-            )
-            return {"successful": True}
-        except ValueError:
-            log.error(
-                "Invalid date range",
-                extra=dict(start_date=start_date, end_date=end_date),
-            )
-            return {"successful": False}
-
-
-RegisteredTimeseriesBackfillTask = celery_app.register_task(TimeseriesBackfillTask())
-timeseries_backfill_task = celery_app.tasks[RegisteredTimeseriesBackfillTask.name]
 
 
 class TimeseriesBackfillCommitsTask(BaseCodecovTask):
