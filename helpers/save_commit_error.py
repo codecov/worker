5fe1acd61594268810e913152b118b3fd70e8518
RulaKhaled <rula@codecov.io>
2022-08-18 17:24:35 +0300

    make sure to check if err exist

--- helpers/save_commit_error.py
+++ helpers/save_commit_error.py
@@ -10,13 +10,20 @@ log = logging.getLogger(__name__)
 def save_repo_bot_error(commit: Commit):
     try:
         db_session = commit.get_db_session()
-        err = CommitError(
-            commit=commit,
-            error_code=CommitErrorTypes.Bot.value.REPO_BOT_INVALID.value,
-            error_params={},
+        code = CommitErrorTypes.Bot.value.REPO_BOT_INVALID.value
+        error_exist = (
+            db_session.query(CommitError)
+            .filter_by(commit=commit, error_code=code)
+            .first()
         )
-        db_session.add(err)
-        db_session.commit()
+        if len(commit.errors) == 0 or not error_exist:
+            err = CommitError(
+                commit=commit,
+                error_code=code,
+                error_params={},
+            )
+            db_session.add(err)
+            db_session.commit()
 
     except:
         log.warning("Error saving bot commit error -repo bot invalid-")
@@ -25,9 +32,15 @@ def save_repo_bot_error(commit: Commit):
 def save_yaml_error(commit: Commit, code):
     try:
         db_session = commit.get_db_session()
-        err = CommitError(commit=commit, error_code=code, error_params={})
-        db_session.add(err)
-        db_session.commit()
+        error_exist = (
+            db_session.query(CommitError)
+            .filter_by(commit=commit, error_code=code)
+            .first()
+        )
+        if len(commit.errors) == 0 or not error_exist:
+            err = CommitError(commit=commit, error_code=code, error_params={})
+            db_session.add(err)
+            db_session.commit()
 
     except:
-        log.warning("Error saving yaml commit error {code}")
+        log.warning("Error saving yaml commit error")
--- helpers/tests/unit/test_commit_error.py
+++ helpers/tests/unit/test_commit_error.py
@@ -23,6 +23,19 @@ class TestSaveCommitError(object):
         assert len(commit.errors) == 1
 
     @pytest.mark.asyncio
+    async def test_save_bot_error_already_saved(self, mocker, dbsession):
+        commit = CommitFactory.create()
+
+        mocked_get_db_session = mocker.patch("tasks.base.get_db_session")
+        mocked_get_db_session.return_value = dbsession
+        commit.get_db_session = mocked_get_db_session
+
+        save_repo_bot_error(commit)
+        save_repo_bot_error(commit)
+
+        assert len(commit.errors) == 1
+
+    @pytest.mark.asyncio
     async def test_save_bot_error_exception(self, mocker, caplog):
         commit = CommitFactory.create()
 
@@ -37,10 +50,28 @@ class TestSaveCommitError(object):
     async def test_save_yaml_error(self, mocker, dbsession):
         commit = CommitFactory.create()
 
+        mocked_get_db_session = mocker.patch("tasks.base.get_db_session")
+        mocked_get_db_session.return_value = dbsession
+        commit.get_db_session = mocked_get_db_session
+
         save_yaml_error(commit, "invalid_yaml")
         save_yaml_error(commit, "repo_bot_invalid")
 
         assert commit.errors
+        assert len(commit.errors) == 2
+
+    @pytest.mark.asyncio
+    async def test_save_yaml_error_already_saved(self, mocker, dbsession):
+        commit = CommitFactory.create()
+
+        mocked_get_db_session = mocker.patch("tasks.base.get_db_session")
+        mocked_get_db_session.return_value = dbsession
+        commit.get_db_session = mocked_get_db_session
+
+        save_yaml_error(commit, "invalid_yaml")
+        save_yaml_error(commit, "invalid_yaml")
+        save_yaml_error(commit, "repo_bot_invalid")
+
         assert len(commit.errors) == 2
 
     @pytest.mark.asyncio
