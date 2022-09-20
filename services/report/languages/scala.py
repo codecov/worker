d579ca3a8f9393103e5ea589d0375515ced78fdd
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2022-09-20 10:02:39 -0300

    Use ReportBuilderSession in ScalaProcessor (#939)

--- services/report/languages/scala.py
+++ services/report/languages/scala.py
@@ -1,10 +1,13 @@
 import typing
 
-from shared.reports.resources import Report, ReportFile
-from shared.reports.types import ReportLine
+from shared.reports.resources import Report
 
 from services.report.languages.base import BaseLanguageProcessor
-from services.report.report_builder import ReportBuilder, ReportBuilderSession
+from services.report.report_builder import (
+    CoverageType,
+    ReportBuilder,
+    ReportBuilderSession,
+)
 
 
 class ScalaProcessor(BaseLanguageProcessor):
@@ -19,17 +22,17 @@ class ScalaProcessor(BaseLanguageProcessor):
 
 
 def from_json(data_dict, report_builder_session: ReportBuilderSession) -> Report:
-    ignored_lines, sessionid = (
-        report_builder_session.ignored_lines,
-        report_builder_session.sessionid,
-    )
+    ignored_lines = report_builder_session.ignored_lines
     for f in data_dict["fileReports"]:
         filename = report_builder_session.path_fixer(f["filename"])
         if filename is None:
             continue
-        _file = ReportFile(filename, ignore=ignored_lines.get(filename))
-        fs = _file.__setitem__
+        _file = report_builder_session.file_class(
+            filename, ignore=ignored_lines.get(filename)
+        )
         for ln, cov in f["coverage"].items():
-            fs(int(ln), ReportLine.create(cov, None, [[sessionid, cov]]))
+            _file[int(ln)] = report_builder_session.create_coverage_line(
+                filename=filename, coverage=cov, coverage_type=CoverageType.line
+            )
         report_builder_session.append(_file)
     return report_builder_session.output_report()
