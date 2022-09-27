5e0caee8518d030c9c7fcd7c6683e8829f6eb719
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2022-09-27 14:40:24 -0300

    introduce reportBuilderSession in flowcover (#967)

--- services/report/languages/flowcover.py
+++ services/report/languages/flowcover.py
@@ -4,7 +4,7 @@ from shared.reports.resources import Report, ReportFile
 from shared.reports.types import ReportLine
 
 from services.report.languages.base import BaseLanguageProcessor
-from services.report.report_builder import ReportBuilder
+from services.report.report_builder import ReportBuilder, ReportBuilderSession
 
 
 class FlowcoverProcessor(BaseLanguageProcessor):
@@ -14,19 +14,21 @@ class FlowcoverProcessor(BaseLanguageProcessor):
     def process(
         self, name: str, content: typing.Any, report_builder: ReportBuilder
     ) -> Report:
-        path_fixer, ignored_lines, sessionid, repo_yaml = (
-            report_builder.path_fixer,
-            report_builder.ignored_lines,
-            report_builder.sessionid,
-            report_builder.repo_yaml,
+        report_builder_session = report_builder.create_report_builder_session(
+            filepath=name
         )
-        return from_json(content, path_fixer, ignored_lines, sessionid)
+        return from_json(content, report_builder_session)
 
 
-def from_json(json, fix, ignored_lines, sessionid):
-    report = Report()
+def from_json(json, report_builder_session: ReportBuilderSession) -> Report:
+    path_fixer, ignored_lines, sessionid = (
+        report_builder_session.path_fixer,
+        report_builder_session.ignored_lines,
+        report_builder_session.sessionid,
+    )
+
     for fn, data in json["files"].items():
-        fn = fix(fn)
+        fn = path_fixer(fn)
         if fn is None:
             continue
 
@@ -54,6 +56,6 @@ def from_json(json, fix, ignored_lines, sessionid):
                 coverage=0, sessions=[[sessionid, 0, None, partials]]
             )
 
-        report.append(_file)
+        report_builder_session.append(_file)
 
-    return report
+    return report_builder_session.output_report()
