518b560e71a94472e043b004480dde20ef5624be
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2022-09-28 14:45:59 -0300

    Introduce Component class in worker (#956)
    * Introduce Component class in worker
    
    Components are a new construct that we're introducing to deal with some of the client's requests and complaints about flags. Read more about components and their motivation [here](https://codecovio.atlassian.net/wiki/spaces/~136505508/pages/1183678477/Component+Idea).
    The YAML schema that supports component management is introduced [by this PR](https://github.com/codecov/shared/pull/312/commits/c7bd48173da914bb16137526015791cb5a3c931c).
    
    In the future we're gonna use them to generate PR status and PR comments (in the worker). To help us work with components in the code this dataclass is being introduced along with some utility methods that I know will be useful for the PR statuses.
    Related ticket: [CODE-2065](https://codecovio.atlassian.net/browse/CODE-2065)
    
    Other comments:
    - `get_display_name` is a series of fallbacks. Component name is the preferred string, component_id is the real ID (required, preferably immutable). They are only for individual components.
    - the preferred and recommended way to create a `Component` instance is feeding the component dict to `Component.from_dict`.
    - Further PRs will make use of this class and make more sense of everything.
    
    * Add utility function to retrieve component definitions from yaml
    
    `get_component_from_yaml` is an utility function that returns `Components` from the yaml.
    It will help avoid code duplication in the PR status and comment with Components, that come from different parts of the code.
    
    * Move components from typings to helpers. Update typehint on get_components_from_yaml
    
    * Use `default_rules` for components correctly.
    
    `default_rules` specify statuses, paths and flag_regexes that should be inherited by individual components in case they don't have some of those keys. They are not a magical defult component on their own.
    
    Any individual component can override any of the default rules by specifying their own key.
    If you only have default_rules and not individual_components they become virtually useless :3
    
    * Remove paths getter function from components.
    
    That function existed because component statuses could have paths. That was removed by [this PR](https://github.com/codecov/shared/pull/312), so we don't need this function anymore.
    
    Also moving the component test file under the unit folder.

--- services/yaml/reader.py
+++ services/yaml/reader.py
@@ -1,8 +1,10 @@
 import logging
 from decimal import ROUND_CEILING, ROUND_FLOOR, ROUND_HALF_EVEN, Decimal
-from typing import Any, Mapping
+from typing import Any, List, Mapping
 
 from shared.yaml.user_yaml import UserYaml
+
+from helpers.components import Component
 
 log = logging.getLogger(__name__)
 
@@ -54,3 +56,22 @@ def get_paths_from_flags(yaml_dict: UserYaml, flags):
         return list(set(res))
     else:
         return []
+
+
+def get_components_from_yaml(yaml: UserYaml) -> List[Component]:
+    component_definitions = read_yaml_field(yaml, ("component_management",))
+    if not component_definitions:
+        return []
+    # Default set of rules that is overriden by individual components.
+    # The individual components inherit the values from default_definition if they don't have a particular key defined in the default rules
+    default_definition = component_definitions.get("default_rules", {})
+
+    individual_components = list(
+        map(
+            lambda component_dict: Component.from_dict(
+                {**default_definition, **component_dict}
+            ),
+            component_definitions.get("individual_components", []),
+        )
+    )
+    return individual_components
