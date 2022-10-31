3078c6613be4b11be9df0bdfe496a862ba15e55c
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2022-10-31 16:13:38 -0300

    Add metrics.timer to `build_payload` function (#988)
    * Add metrics.timer to `build_payload` function
    
    Related ticket: [CODE-2304](https://codecovio.atlassian.net/browse/CODE-2304)
    
    Investigating recent issues that a self-hosted customer is having we felt a lack of monitoring data in the build_payload process of checks notifiers. Adding metrics.timer decorator to the build_payload function will help us get more visibility on exactly what part of the notification process might be taking longer than expected.
    
    * Move metrics.timer inside async function
    
    There's a reasonable chance that doing `@metrics.timer()` on a `async` function will return only the time to generate the `Coroutine` that is the async function and not the time taken to actually run it (which happens when you `await` the async function).
    
    So to be sure that we are timing the right thing the best is to put the timer inside the async part of the function.

--- services/notification/notifiers/checks/patch.py
+++ services/notification/notifiers/checks/patch.py
@@ -1,4 +1,5 @@
 from database.enums import Notification
+from helpers.metrics import metrics
 from services.notification.notifiers.base import Comparison
 from services.notification.notifiers.checks.base import ChecksNotifier
 from services.notification.notifiers.mixins.status import StatusPatchMixin
@@ -20,49 +21,52 @@ class PatchChecksNotifier(StatusPatchMixin, ChecksNotifier):
         We only add annotaions to the top-level patch check of a project.
         We do not add annotations on checks that are used with paths/flags
         """
-        state, message = await self.get_patch_status(comparison)
-        codecov_link = self.get_codecov_pr_link(comparison)
-
-        title = message
-
-        should_use_upgrade = self.should_use_upgrade_decoration()
-        if should_use_upgrade:
-            message = self.get_upgrade_message(comparison)
-            title = "Codecov Report"
-
-        checks_yaml_field = read_yaml_field(self.current_yaml, ("github_checks",))
-
-        should_annotate = (
-            checks_yaml_field.get("annotations", True)
-            if checks_yaml_field is not None
-            else True
-        )
-
-        flags = self.notifier_yaml_settings.get("flags")
-        paths = self.notifier_yaml_settings.get("paths")
-        if (
-            flags is not None
-            or paths is not None
-            or should_use_upgrade
-            or should_annotate is False
+        with metrics.timer(
+            "worker.services.notifications.notifiers.checks.patch.build_payload"
         ):
+            state, message = await self.get_patch_status(comparison)
+            codecov_link = self.get_codecov_pr_link(comparison)
+
+            title = message
+
+            should_use_upgrade = self.should_use_upgrade_decoration()
+            if should_use_upgrade:
+                message = self.get_upgrade_message(comparison)
+                title = "Codecov Report"
+
+            checks_yaml_field = read_yaml_field(self.current_yaml, ("github_checks",))
+
+            should_annotate = (
+                checks_yaml_field.get("annotations", True)
+                if checks_yaml_field is not None
+                else True
+            )
+
+            flags = self.notifier_yaml_settings.get("flags")
+            paths = self.notifier_yaml_settings.get("paths")
+            if (
+                flags is not None
+                or paths is not None
+                or should_use_upgrade
+                or should_annotate is False
+            ):
+                return {
+                    "state": state,
+                    "output": {
+                        "title": f"{title}",
+                        "summary": "\n\n".join([codecov_link, message]),
+                    },
+                }
+            diff = await self.get_diff(comparison)
+            #  TODO: Look into why the apply diff in get_patch_status is not saving state at this point
+            comparison.head.report.apply_diff(diff)
+            annotations = self.create_annotations(comparison, diff)
+
             return {
                 "state": state,
                 "output": {
                     "title": f"{title}",
                     "summary": "\n\n".join([codecov_link, message]),
+                    "annotations": annotations,
                 },
             }
-        diff = await self.get_diff(comparison)
-        #  TODO: Look into why the apply diff in get_patch_status is not saving state at this point
-        comparison.head.report.apply_diff(diff)
-        annotations = self.create_annotations(comparison, diff)
-
-        return {
-            "state": state,
-            "output": {
-                "title": f"{title}",
-                "summary": "\n\n".join([codecov_link, message]),
-                "annotations": annotations,
-            },
-        }
