303327271efd397a64d510c79132c091d02e2029
trent-codecov <trent@codecov.io>
2022-12-06 22:20:10 -0600

    Add init to signal init

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import logging
 import logging.config
 
-from celery import Celery
+from celery import Celery, signals
 
 from helpers.logging_config import get_logging_config_dict
 from helpers.sentry import initialize_sentry, is_sentry_enabled
 
 log = logging.getLogger(__name__)
 
-
 _config_dict = get_logging_config_dict()
 logging.config.dictConfig(_config_dict)
 
-if is_sentry_enabled():
-    initialize_sentry()
-
 celery_app = Celery("tasks")
 celery_app.config_from_object("celery_config:CeleryWorkerConfig")
+
+
+@signals.celeryd_init.connect
+def init_sentry(**_kwargs):
+    if is_sentry_enabled():
+        initialize_sentry()
