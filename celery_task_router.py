ff4c26caad1657738868dd54d44b720e6dba33c0
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2022-12-12 11:45:41 -0300

    Revert "Revert (#1017) - upload finisher task is failing the dynamic routing (#1018)" (#1019)
    This reverts commit 28f8791442d5c9d9f5df7add414c1decf5e944ed.
    And re-enables the dynamic task router.... again.
    
    This time the UploadFinisherTask, when calling ComputeComparison task was passing the arguments via `args` instead of `kwargs` as everywhere else. This breaks the expectations of the task router (that depends on `kwargs` to find the user plan).
    
    Since the trend all over the worker is to pass arguments via `kwargs` I don't think it's a problem if we just fix this one place. If needed we can look into making the task router more robust, though.

---
+++ celery_task_router.py
@@ -0,0 +1,118 @@
+import shared.celery_config as shared_celery_config
+from shared.billing import BillingPlan
+from shared.celery_router import route_tasks_based_on_user_plan
+
+from database.engine import get_db_session
+from database.models.core import Commit, CompareCommit, Owner, Repository
+from database.models.profiling import ProfilingCommit, ProfilingUpload
+
+
+def _get_user_plan_from_ownerid(db_session, ownerid, *args, **kwargs) -> str:
+    result = db_session.query(Owner.plan).filter(Owner.ownerid == ownerid).first()
+    if result:
+        return result.plan
+    return BillingPlan.users_basic.db_name
+
+
+def _get_user_plan_from_repoid(db_session, repoid, *args, **kwargs) -> str:
+    result = (
+        db_session.query(Owner.plan)
+        .join(Repository.owner)
+        .filter(Repository.repoid == repoid)
+        .first()
+    )
+    if result:
+        return result.plan
+    return BillingPlan.users_basic.db_name
+
+
+def _get_user_plan_from_org_ownerid(dbsession, org_ownerid, *args, **kwargs) -> str:
+    return _get_user_plan_from_ownerid(dbsession, ownerid=org_ownerid)
+
+
+def _get_user_plan_from_profiling_commit(
+    dbsession, profiling_id, *args, **kwargs
+) -> str:
+    result = (
+        dbsession.query(Owner.plan)
+        .join(ProfilingCommit.repository)
+        .join(Repository.owner)
+        .filter(ProfilingCommit.id == profiling_id)
+        .first()
+    )
+    if result:
+        return result.plan
+    return BillingPlan.users_basic.db_name
+
+
+def _get_user_plan_from_profiling_upload(
+    dbsession, profiling_upload_id, *args, **kwargs
+) -> str:
+    result = (
+        dbsession.query(Owner.plan)
+        .join(ProfilingUpload.profiling_commit)
+        .join(ProfilingCommit.repository)
+        .join(Repository.owner)
+        .filter(ProfilingUpload.id == profiling_upload_id)
+        .first()
+    )
+    if result:
+        return result.plan
+    return BillingPlan.users_basic.db_name
+
+
+def _get_user_plan_from_comparison_id(dbsession, comparison_id, *args, **kwargs) -> str:
+    result = (
+        dbsession.query(Owner.plan)
+        .join(CompareCommit.compare_commit)
+        .join(Commit.repository)
+        .join(Repository.owner)
+        .filter(CompareCommit.id_ == comparison_id)
+        .first()
+    )
+    if result:
+        return result.plan
+    return BillingPlan.users_basic.db_name
+
+
+def _get_user_plan_from_task(dbsession, task_name: str, task_kwargs: dict) -> str:
+    owner_plan_lookup_funcs = {
+        # from ownerid
+        shared_celery_config.delete_owner_task_name: _get_user_plan_from_ownerid,
+        shared_celery_config.add_to_sendgrid_list_task_name: _get_user_plan_from_ownerid,
+        shared_celery_config.send_email_task_name: _get_user_plan_from_ownerid,
+        shared_celery_config.sync_repos_task_name: _get_user_plan_from_ownerid,
+        shared_celery_config.sync_teams_task_name: _get_user_plan_from_ownerid,
+        # from org_ownerid
+        shared_celery_config.new_user_activated_task_name: _get_user_plan_from_org_ownerid,
+        # from repoid
+        shared_celery_config.upload_task_name: _get_user_plan_from_repoid,
+        shared_celery_config.upload_processor_task_name: _get_user_plan_from_repoid,
+        shared_celery_config.notify_task_name: _get_user_plan_from_repoid,
+        shared_celery_config.commit_update_task_name: _get_user_plan_from_repoid,
+        shared_celery_config.flush_repo_task_name: _get_user_plan_from_repoid,
+        shared_celery_config.status_set_error_task_name: _get_user_plan_from_repoid,
+        shared_celery_config.status_set_pending_task_name: _get_user_plan_from_repoid,
+        shared_celery_config.pulls_task_name: _get_user_plan_from_repoid,
+        shared_celery_config.upload_finisher_task_name: _get_user_plan_from_repoid,  # didn't want to directly import the task module
+        # from profiling_commitid
+        shared_celery_config.profiling_collection_task_name: _get_user_plan_from_profiling_commit,
+        shared_celery_config.profiling_summarization_task_name: _get_user_plan_from_profiling_commit,
+        # from profiling_upload_id
+        shared_celery_config.profiling_normalization_task_name: _get_user_plan_from_profiling_upload,
+        # from comparison_id
+        shared_celery_config.compute_comparison_task_name: _get_user_plan_from_comparison_id,
+    }
+    func_to_use = owner_plan_lookup_funcs.get(
+        task_name, lambda *args, **kwargs: BillingPlan.users_basic.db_name
+    )
+    return func_to_use(dbsession, **task_kwargs)
+
+
+def route_task(name, args, kwargs, options, task=None, **kw):
+    """Function to dynamically route tasks to the proper queue.
+    Docs: https://docs.celeryq.dev/en/stable/userguide/routing.html#routers
+    """
+    db_session = get_db_session()
+    user_plan = _get_user_plan_from_task(db_session, name, kwargs)
+    return route_tasks_based_on_user_plan(name, user_plan)
