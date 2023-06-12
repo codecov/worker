97f80de0dd8f73d1624b7e96a201fa2adc2c97c6
Scott Nelson <scott@codecov.io>
2023-06-12 09:58:57 -0400

    Set test RUN_ENV to DEV

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import mock
@@ -22,6 +23,7 @@ def pytest_configure(config):
     This hook is called for every plugin and initial conftest
     file after command line options have been parsed.
     """
+    os.environ["RUN_ENV"] = "DEV"
     initialize_logging()
 
 
