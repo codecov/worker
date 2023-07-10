5fc4c0c46fda5e08f42d2e0a6035c801614417d4
Rylee Fox <rylee.fox@sentry.io>
2023-07-10 16:41:15 -0700

    fix tests?

--- tasks/tests/unit/test_upload_task.py
+++ tasks/tests/unit/test_upload_task.py
@@ -1167,7 +1167,7 @@ class TestUploadTaskUnit(object):
             "Codecov Webhook. None",
             "None/webhooks/github",
             ["pull_request", "delete", "push", "public", "status", "repository"],
-            "test46nudghi6oft49bay37keyiuhok7",
+            "ab164bf3f7d947f2a0681b215404873e",
             token=None,
         )
 
