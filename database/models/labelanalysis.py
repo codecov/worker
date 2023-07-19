86edcdd1fd876eb3199345303b6c62dc2ebb8dad
Gguidini <giovanni@codecov.io>
2023-07-19 18:34:16 +0200

    Add `LabelAnalysisProcessingErrors`
    Currently it's hard for customers of ATS to self support when we are missing data.
    This usually leads to the customer seeing all labels as absent, even though their setup is apparently commit. It's usually because we are missing data.
    
    By including error messages (using `LabelAnalysisProcessingErrors`) we can give them more information about what is wrong, so they can better self support.

--- database/models/labelanalysis.py
+++ database/models/labelanalysis.py
@@ -1,8 +1,16 @@
+from enum import Enum
+
 from sqlalchemy import Column, ForeignKey, types
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import relationship
 
 from database.base import CodecovBaseModel, MixinBaseClass
+
+
+class LabelAnalysisProcessingErrorCode(Enum):
+    NOT_FOUND = "not found"
+    FAILED = "failed"
+    MISSING_DATA = "missing data"
 
 
 class LabelAnalysisRequest(CodecovBaseModel, MixinBaseClass):
@@ -15,3 +23,18 @@ class LabelAnalysisRequest(CodecovBaseModel, MixinBaseClass):
     # relationships
     base_commit = relationship("Commit", foreign_keys=[base_commit_id])
     head_commit = relationship("Commit", foreign_keys=[head_commit_id])
+
+
+class LabelAnalysisProcessingError(CodecovBaseModel, MixinBaseClass):
+    __tablename__ = "labelanalysis_labelanalysisprocessingerror"
+    label_analysis_request = relationship(LabelAnalysisRequest, backref="errors")
+    label_analysis_request_id = Column(
+        "label_analysis_request_id",
+        types.BigInteger,
+        ForeignKey("labelanalysis_labelanalysisrequest.id"),
+    )
+    error_code = Column(types.String(100), nullable=False)
+    error_params = Column(postgresql.JSON, nullable=True)
+
+    def to_representation(self):
+        return dict(error_code=self.error_code, error_params=self.error_params)
