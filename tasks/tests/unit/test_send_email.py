d8a7d5a42e65f7e68ba44ad4bb4a6dd58709a4d6
joseph-sentry <joseph.sawaya@sentry.io>
2023-09-12 17:24:16 -0400

    Remove old send_email task and replace it
    Remove old send_email task functionality.
    
    send_email task uses TemplateService and SMTPService
    to render provided template and send email to specified
    Owner.
    
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- tasks/tests/unit/test_send_email.py
+++ tasks/tests/unit/test_send_email.py
@@ -2,100 +2,86 @@ import json
 from pathlib import Path
 
 import pytest
+from jinja2 import TemplateNotFound, UndefinedError
+from shared.utils.test_utils.mock_metrics import mock_metrics as utils_mock_metrics
 
 from database.tests.factories import OwnerFactory
 from tasks.send_email import SendEmailTask
 
 here = Path(__file__)
 
+to_addr = "test_to@codecov.io"
+
 
 class TestSendEmailTask(object):
     @pytest.mark.asyncio
-    async def test_end_of_trial_email_with_list_type(
+    async def test_send_email(
         self,
         mocker,
         mock_configuration,
         dbsession,
         codecov_vcr,
         mock_storage,
+        mock_smtp,
         mock_redis,
     ):
-        owner = OwnerFactory.create(email="felipe@codecov.io")
+        metrics = utils_mock_metrics(mocker)
+        owner = OwnerFactory.create(email=to_addr)
         dbsession.add(owner)
         dbsession.flush()
         result = await SendEmailTask().run_async(
-            db_session=dbsession, ownerid=owner.ownerid, list_type="end-of-trial"
+            db_session=dbsession,
+            ownerid=owner.ownerid,
+            from_addr="test_from@codecov.io",
+            template_name="test",
+            username="test_username",
         )
-        assert result["job_id"] == "9791f6a7-3d3b-4ae9-8f71-67bd98f33008"
+
+        assert metrics.data["worker.tasks.send_email.attempt"] == 1
+        assert metrics.data["worker.tasks.send_email.succeed"] == 1
+        assert metrics.data["worker.tasks.send_email.fail"] == 0
 
     @pytest.mark.asyncio
-    async def test_end_of_trial_email_with_email_type(
-        self,
-        mocker,
-        mock_configuration,
-        dbsession,
-        codecov_vcr,
-        mock_storage,
-        mock_redis,
+    async def test_send_email_non_existent_template(
+        self, mocker, mock_configuration, dbsession, codecov_vcr, mock_smtp
     ):
-        owner = OwnerFactory.create(email="felipe@codecov.io")
+        to_addr = "test_to@codecov.io"
+        owner = OwnerFactory.create(email=to_addr)
         dbsession.add(owner)
         dbsession.flush()
-        result = await SendEmailTask().run_async(
-            db_session=dbsession, ownerid=owner.ownerid, email_type="end-of-trial"
-        )
-        assert result["job_id"] == "9791f6a7-3d3b-4ae9-8f71-67bd98f33008"
+        with pytest.raises(TemplateNotFound):
+            result = await SendEmailTask().run_async(
+                db_session=dbsession,
+                ownerid=owner.ownerid,
+                from_addr="test_from@codecov.io",
+                template_name="non_existent",
+                username="test_username",
+            )
 
     @pytest.mark.asyncio
-    async def test_send_email_invalid_owner(
-        self, mocker, mock_configuration, dbsession, codecov_vcr
+    async def test_send_email_missing_kwargs(
+        self, mocker, mock_configuration, dbsession, codecov_vcr, mock_smtp
     ):
-        result = await SendEmailTask().run_async(
-            db_session=dbsession, ownerid=999999999, list_type="end-of-trial"
-        )
-        assert result is None
-
-    @pytest.mark.asyncio
-    async def test_send_email_invalid_list_with_list_type(
-        self, mocker, mock_configuration, dbsession, codecov_vcr
-    ):
-        owner = OwnerFactory.create(email="felipe@codecov.io")
+        to_addr = "test_to@codecov.io"
+        owner = OwnerFactory.create(email=to_addr)
         dbsession.add(owner)
         dbsession.flush()
-        result = await SendEmailTask().run_async(
-            db_session=dbsession, ownerid=owner.ownerid, list_type="fake-list"
-        )
-        assert result is None
-
-    @pytest.mark.asyncio
-    async def test_send_email_invalid_list_with_email_type(
-        self, mocker, mock_configuration, dbsession, codecov_vcr
-    ):
-        owner = OwnerFactory.create(email="felipe@codecov.io")
-        dbsession.add(owner)
-        dbsession.flush()
-        result = await SendEmailTask().run_async(
-            db_session=dbsession, ownerid=owner.ownerid, email_type="fake-list"
-        )
-        assert result is None
-
-    @pytest.mark.asyncio
-    async def test_send_email_no_list(
-        self, mocker, mock_configuration, dbsession, codecov_vcr
-    ):
-        owner = OwnerFactory.create(email="felipe@codecov.io")
-        dbsession.add(owner)
-        dbsession.flush()
-        result = await SendEmailTask().run_async(
-            db_session=dbsession, ownerid=owner.ownerid
-        )
-        assert result is None
+        with pytest.raises(UndefinedError):
+            result = await SendEmailTask().run_async(
+                db_session=dbsession,
+                ownerid=owner.ownerid,
+                from_addr="test_from@codecov.io",
+                template_name="test",
+            )
 
     @pytest.mark.asyncio
     async def test_send_email_invalid_owner_no_list_type(
-        self, mocker, mock_configuration, dbsession, codecov_vcr
+        self, mocker, mock_configuration, dbsession, codecov_vcr, mock_smtp
     ):
         result = await SendEmailTask().run_async(
-            db_session=dbsession, ownerid=999999999
+            db_session=dbsession,
+            ownerid=99999999,
+            from_addr="test_from@codecov.io",
+            template_name="test",
+            username="test_username",
         )
-        assert result is None
