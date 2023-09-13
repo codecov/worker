e045d9286099f22d8d1fa659f0fd7f87105ddd79
Trent Schmidt <trent@codecov.io>
2023-09-13 00:42:54 -0400

    Working on GHA CI

--- worker.sh
+++ worker.sh
@@ -1,1 +1,13 @@
-python main.py worker --queue new_tasks,upload,celery
+#!/bin/sh
+
+queues=""
+if [[ "$CODECOV_WORKER_QUEUES" ]]; then
+  queues="--queue $CODECOV_WORKER_QUEUES"
+fi
+
+if [ -z "$1" ];
+then
+  python main.py worker ${queues}
+else
+  exec "$@"
+fi
