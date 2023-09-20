5fec16d3f508db3e81677fe3a6909d99df6ae1ad
joseph-sentry <joseph.sawaya@sentry.io>
2023-09-20 12:51:43 -0400

    Simplify email wrapper
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- helpers/email.py
+++ helpers/email.py
@@ -6,48 +6,8 @@ class Email:
         self, to_addr=None, from_addr=None, subject=None, text=None, html=None
     ):
         self.message = EmailMessage()
-        self.to_addr = to_addr
-        self.from_addr = from_addr
-        self.text = text
-        self.html = html
-        self.subject = subject
-
-    @property
-    def to_addr(self):
-        return self.message["To"]
-
-    @to_addr.setter
-    def to_addr(self, val):
-        self.message["To"] = val
-
-    @property
-    def from_addr(self):
-        return self.message["From"]
-
-    @from_addr.setter
-    def from_addr(self, val):
-        self.message["From"] = val
-
-    @property
-    def subject(self):
-        return self.message["Subject"]
-
-    @subject.setter
-    def subject(self, val):
-        self.message["Subject"] = val
-
-    @property
-    def text(self):
-        return self.message.get_payload()[0]
-
-    @text.setter
-    def text(self, val):
-        self.message.set_content(val)
-
-    @property
-    def html(self):
-        return self.message.get_payload()[1]
-
-    @html.setter
-    def html(self, val):
-        self.message.add_alternative(val, "text/html")
+        self.message["To"] = to_addr
+        self.message["From"] = from_addr
+        self.message["Subject"] = subject
+        self.message.set_content(text)
+        self.message.add_alternative(html, "text/html")
