f5c2107f1d87ec7e30ea5b185a9584c364cc58c0
Matt Hammerly <matt.hammerly@sentry.io>
2023-09-28 10:02:30 -0700

    perf/feat: use new generator variants of list_repos in sync_repos

---
+++ rollouts/__init__.py
@@ -0,0 +1,24 @@
+from shared.rollouts import Feature, FeatureVariant
+
+from database.models import Owner, Repository
+
+
+def owner_slug(owner: Owner) -> str:
+    return f"{owner.service}/{owner.username}"
+
+
+def repo_slug(repo: Repository) -> str:
+    return f"{repo.service}/{repo.owner.username}/{repo.name}"
+
+
+# By default, features have one variant:
+#    { "enabled": FeatureVariant(True, 1.0) }
+LIST_REPOS_GENERATOR_BY_OWNER_SLUG = Feature(
+    "list_repos_generator",
+    0.0,
+    overrides={
+        "github/codecov": "enabled",
+        "bitbucket/codecov": "enabled",
+        "gitlab/codecov": "enabled",
+    },
+)
