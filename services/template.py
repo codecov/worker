23f0a53b546ce94e5b6124e437e5c6b3558ad618
joseph-sentry <joseph.sawaya@sentry.io>
2023-09-29 13:39:33 -0400

    small fix to TemplateService
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- services/template.py
+++ services/template.py
@@ -9,12 +9,13 @@ class TemplateService:
         return cls.env is not None
 
     def __init__(self):
-        # this loads the templates from the templates directory in this repository since it's looking for a dir named `templates` next to app.py
-        TemplateService.env = Environment(
-            loader=PackageLoader("app"),
-            autoescape=select_autoescape(),
-            undefined=StrictUndefined,
-        )
+        if TemplateService.env is None:
+            # this loads the templates from the templates directory in this repository since it's looking for a dir named `templates` next to app.py
+            TemplateService.env = Environment(
+                loader=PackageLoader("app"),
+                autoescape=select_autoescape(),
+                undefined=StrictUndefined,
+            )
 
     def get_template(self, name):
         template = TemplateService.env.get_template(f"{name}")
