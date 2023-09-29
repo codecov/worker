9238ccb64c8fe91c3c6c87618a67922f5de31fcb
joseph-sentry <joseph.sawaya@sentry.io>
2023-09-29 10:18:16 -0400

    make a singleton again, but without get_template_service
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- services/template.py
+++ services/template.py
@@ -2,14 +2,20 @@ from jinja2 import Environment, PackageLoader, StrictUndefined, select_autoescap
 
 
 class TemplateService:
+    env = None
+
+    @classmethod
+    def loaded(cls):
+        return cls.env is not None
+
     def __init__(self):
         # this loads the templates from the templates directory in this repository since it's looking for a dir named `templates` next to app.py
-        self.env = Environment(
+        TemplateService.env = Environment(
             loader=PackageLoader("app"),
             autoescape=select_autoescape(),
             undefined=StrictUndefined,
         )
 
     def get_template(self, name):
-        template = self.env.get_template(f"{name}")
+        template = TemplateService.env.get_template(f"{name}")
         return template
