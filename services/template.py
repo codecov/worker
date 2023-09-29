e570cc6ef42110f8ffade4074095d86ae885ed11
joseph-sentry <joseph.sawaya@sentry.io>
2023-09-29 14:19:32 -0400

    Remove loaded method from template service
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- services/template.py
+++ services/template.py
@@ -4,10 +4,6 @@ from jinja2 import Environment, PackageLoader, StrictUndefined, select_autoescap
 class TemplateService:
     env = None
 
-    @classmethod
-    def loaded(cls):
-        return cls.env is not None
-
     def __init__(self):
         if TemplateService.env is None:
             # this loads the templates from the templates directory in this repository since it's looking for a dir named `templates` next to app.py
