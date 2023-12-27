ad8af4e5f632fa17a162b055456d0687a5c94e23
Scott Nelson <scott@codecov.io>
2023-12-27 11:46:52 -0500

    feat: Bundle analysis notify task

---
+++ tasks/bundle_analysis_notify.py
@@ -0,0 +1,122 @@
+import logging
+from typing import Any, Dict
+
+from shared.celery_config import notify_task_name
+from shared.yaml import UserYaml
+
+from app import celery_app
+from database.enums import ReportType
+from database.models import Commit
+from services.bundle_analysis import Notifier
+from services.lock_manager import LockManager, LockRetry, LockType
+from tasks.base import BaseCodecovTask
+
+log = logging.getLogger(__name__)
+
+bundle_analysis_notify_task_name = "app.tasks.bundle_analysis.BundleAnalysisNotify"
+
+
+class BundleAnalysisNotifyTask(BaseCodecovTask, name=bundle_analysis_notify_task_name):
+    async def run_async(
+        self,
+        db_session,
+        # Celery `chain` injects this argument - it's the returned result
+        # from the prior task in the chain
+        previous_result: Dict[str, Any],
+        *,
+        repoid: int,
+        commitid: str,
+        commit_yaml: dict,
+        **kwargs,
+    ):
+        repoid = int(repoid)
+        commit_yaml = UserYaml.from_dict(commit_yaml)
+
+        log.info(
+            "Starting bundle analysis notify",
+            extra=dict(
+                repoid=repoid,
+                commit=commitid,
+                commit_yaml=commit_yaml,
+            ),
+        )
+
+        lock_manager = LockManager(
+            repoid=repoid,
+            commitid=commitid,
+            report_type=ReportType.BUNDLE_ANALYSIS,
+        )
+
+        try:
+            with lock_manager.locked(
+                LockType.BUNDLE_ANALYSIS_NOTIFY,
+                retry_num=self.request.retries,
+            ):
+                return await self.process_async_within_lock(
+                    db_session=db_session,
+                    repoid=repoid,
+                    commitid=commitid,
+                    commit_yaml=commit_yaml,
+                    previous_result=previous_result,
+                    **kwargs,
+                )
+        except LockRetry as retry:
+            self.retry(max_retries=5, countdown=retry.countdown)
+
+    async def process_async_within_lock(
+        self,
+        *,
+        db_session,
+        repoid: int,
+        commitid: str,
+        commit_yaml: UserYaml,
+        previous_result: Dict[str, Any],
+        **kwargs,
+    ):
+        log.info(
+            "Running bundle analysis notify",
+            extra=dict(
+                repoid=repoid,
+                commit=commitid,
+                commit_yaml=commit_yaml,
+                parent_task=self.request.parent_id,
+            ),
+        )
+
+        commit = (
+            db_session.query(Commit).filter_by(repoid=repoid, commitid=commitid).first()
+        )
+        assert commit, "commit not found"
+
+        notify = True
+
+        # these are the task results from prior processor tasks in the chain
+        # (they get accumulated as we execute each task in succession)
+        processing_results = previous_result.get("results", [])
+
+        if all((result["error"] is not None for result in processing_results)):
+            # every processor errored, nothing to notify on
+            notify = False
+
+        success = None
+        if notify:
+            notifier = Notifier(commit, commit_yaml)
+            success = await notifier.notify()
+
+        log.info(
+            "Finished bundle analysis notify",
+            extra=dict(
+                repoid=repoid,
+                commit=commitid,
+                commit_yaml=commit_yaml,
+                parent_task=self.request.parent_id,
+            ),
+        )
+
+        return {"notify_attempted": notify, "notify_succeeded": success}
+
+
+RegisteredBundleAnalysisNotifyTask = celery_app.register_task(
+    BundleAnalysisNotifyTask()
+)
+bundle_analysis_notify_task = celery_app.tasks[RegisteredBundleAnalysisNotifyTask.name]
