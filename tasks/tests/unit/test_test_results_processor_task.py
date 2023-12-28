071e5a0eb4140e70c72722843d0290cb4cd266c6
joseph-sentry <joseph.sawaya@sentry.io>
2023-12-28 14:32:49 -0500

    test: add tests for the test result processor task
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

---
+++ tasks/tests/unit/test_test_results_processor_task.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+
+import pytest
+from celery.exceptions import Retry
+
+from database.models import CommitReport
+from database.tests.factories import CommitFactory, UploadFactory
+from services.archive import ArchiveService
+from tasks.test_results_processor import TestResultsProcessorTask
+
+here = Path(__file__)
+
+
+class TestUploadTestProcessorTask(object):
+    @pytest.mark.asyncio
+    @pytest.mark.integration
+    async def test_upload_processor_task_call(
+        self,
+        mocker,
+        mock_configuration,
+        dbsession,
+        codecov_vcr,
+        mock_storage,
+        mock_redis,
+        celery_app,
+    ):
+        url = "v4/raw/2019-05-22/C3C4715CA57C910D11D5EB899FC86A7E/4c4e4654ac25037ae869caeb3619d485970b6304/a84d445c-9c1e-434f-8275-f18f1f320f81.txt"
+        with open(here.parent.parent / "samples" / "sample_test.txt") as f:
+            content = f.read()
+            mock_storage.write_file("archive", url, content)
+        upload = UploadFactory.create(storage_path=url)
+        dbsession.add(upload)
+        dbsession.flush()
+        redis_queue = [{"url": url, "upload_pk": upload.id_}]
+        mocker.patch.object(TestResultsProcessorTask, "app", celery_app)
+
+        commit = CommitFactory.create(
+            message="hello world",
+            commitid="cd76b0821854a780b60012aed85af0a8263004ad",
+            repository__owner__unencrypted_oauth_token="test7lk5ndmtqzxlx06rip65nac9c7epqopclnoy",
+            repository__owner__username="joseph-sentry",
+            repository__owner__service="github",
+            repository__name="codecov-demo",
+        )
+        dbsession.add(commit)
+        dbsession.flush()
+        current_report_row = CommitReport(commit_id=commit.id_)
+        dbsession.add(current_report_row)
+        dbsession.flush()
+        result = await TestResultsProcessorTask().run_async(
+            dbsession,
+            repoid=commit.repoid,
+            commitid=commit.commitid,
+            commit_yaml={"codecov": {"max_report_age": False}},
+            arguments_list=redis_queue,
+        )
+        expected_result = {
+            "successful": True,
+            "testrun_list": [
+                {
+                    "duration": 0.001,
+                    "name": "api.temp.calculator.test_calculator::test_add",
+                    "outcome": "Outcome.Pass",
+                    "testsuite": "pytest",
+                },
+                {
+                    "duration": 0.001,
+                    "name": "api.temp.calculator.test_calculator::test_subtract",
+                    "outcome": "Outcome.Pass",
+                    "testsuite": "pytest",
+                },
+                {
+                    "duration": 0.0,
+                    "name": "api.temp.calculator.test_calculator::test_multiply",
+                    "outcome": "Outcome.Pass",
+                    "testsuite": "pytest",
+                },
+                {
+                    "duration": 0.001,
+                    "name": "api.temp.calculator.test_calculator::test_divide",
+                    "outcome": "Outcome.Failure",
+                    "testsuite": "pytest",
+                },
+            ],
+        }
+        assert expected_result == result
+        assert commit.message == "hello world"
