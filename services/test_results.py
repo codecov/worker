3b2089f5102b1e96c1b9f567da77507f88a6a875
joseph-sentry <joseph.sawaya@sentry.io>
2023-12-28 12:58:39 -0500

    feat: add test results report service
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

---
+++ services/test_results.py
@@ -0,0 +1,34 @@
+from typing import Mapping, Sequence
+
+
+from database.enums import ReportType
+from database.models import Commit, CommitReport
+from services.report import BaseReportService
+
+
+class TestResultsReportService(BaseReportService):
+    async def initialize_and_save_report(
+        self, commit: Commit, report_code: str = None
+    ) -> CommitReport:
+        db_session = commit.get_db_session()
+        current_report_row = (
+            db_session.query(CommitReport)
+            .filter_by(
+                commit_id=commit.id_,
+                code=report_code,
+                report_type=ReportType.TEST_RESULTS.value,
+            )
+            .first()
+        )
+        if not current_report_row:
+            # This happens if the commit report is being created for the first time
+            # or backfilled
+            current_report_row = CommitReport(
+                commit_id=commit.id_,
+                code=report_code,
+                report_type=ReportType.TEST_RESULTS.value,
+            )
+            db_session.add(current_report_row)
+            db_session.flush()
+
+        return current_report_row
