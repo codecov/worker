7366d0c356b40066aa8d8e52eeb3a9241846fd7e
Adrian <adrian@codecov.io>
2024-01-19 17:22:26 -0600

    test: add test for no bundle changes

--- services/tests/test_bundle_analysis.py
+++ services/tests/test_bundle_analysis.py
@@ -27,6 +27,9 @@ Changes will decrease total bundle size by 3.46KB :arrow_down:
 | ----------- | ---- | ------ |
 | test-bundle | 123.46KB | 3.46KB :arrow_down: |"""
 
+expected_message_unchanged = """## Bundle Report
+
+Bundle size has no change :white_check_mark:"""
 
 class MockBundleReport:
     def total_size(self):
@@ -209,6 +212,83 @@ async def test_bundle_analysis_notify_size_decrease(
     assert success == True
     mock_repo_provider.post_comment.assert_called_once_with(
         pull.pullid, expected_message_decrease
+    )
+
+@pytest.mark.asyncio
+async def test_bundle_analysis_notify_size_unchanged(
+    dbsession, mocker, mock_storage, mock_repo_provider
+):
+    base_commit = CommitFactory()
+    dbsession.add(base_commit)
+    base_commit_report = CommitReport(
+        commit=base_commit, report_type=ReportType.BUNDLE_ANALYSIS.value
+    )
+    dbsession.add(base_commit_report)
+
+    head_commit = CommitFactory(repository=base_commit.repository)
+    dbsession.add(head_commit)
+    head_commit_report = CommitReport(
+        commit=head_commit, report_type=ReportType.BUNDLE_ANALYSIS.value
+    )
+    dbsession.add(head_commit_report)
+
+    pull = PullFactory(
+        repository=base_commit.repository,
+        head=head_commit.commitid,
+        base=base_commit.commitid,
+        compared_to=base_commit.commitid,
+    )
+    dbsession.add(pull)
+    dbsession.commit()
+
+    notifier = Notifier(head_commit, UserYaml.from_dict({}))
+
+    repo_key = ArchiveService.get_archive_hash(base_commit.repository)
+    mock_storage.write_file(
+        get_bucket_name(),
+        f"v1/repos/{repo_key}/{base_commit_report.external_id}/bundle_report.sqlite",
+        "test-content",
+    )
+    mock_storage.write_file(
+        get_bucket_name(),
+        f"v1/repos/{repo_key}/{head_commit_report.external_id}/bundle_report.sqlite",
+        "test-content",
+    )
+
+    mocker.patch(
+        "services.bundle_analysis.get_appropriate_storage_service",
+        return_value=mock_storage,
+    )
+    mocker.patch("shared.bundle_analysis.report.BundleAnalysisReport._setup")
+    mocker.patch(
+        "services.bundle_analysis.get_repo_provider_service",
+        return_value=mock_repo_provider,
+    )
+
+    bundle_changes = mocker.patch(
+        "shared.bundle_analysis.comparison.BundleAnalysisComparison.bundle_changes"
+    )
+    bundle_changes.return_value = [
+        BundleChange("test-bundle", BundleChange.ChangeType.CHANGED, size_delta=0),
+    ]
+
+    bundle_report = mocker.patch(
+        "shared.bundle_analysis.report.BundleAnalysisReport.bundle_report"
+    )
+    bundle_report.return_value = MockBundleReport()
+
+    fetch_pr = mocker.patch(
+        "services.bundle_analysis.fetch_and_update_pull_request_information_from_commit"
+    )
+    fetch_pr.return_value = EnrichedPull(
+        database_pull=pull,
+        provider_pull={},
+    )
+
+    success = await notifier.notify()
+    assert success == True
+    mock_repo_provider.post_comment.assert_called_once_with(
+        pull.pullid, expected_message_unchanged
     )
 
 
