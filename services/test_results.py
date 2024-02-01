0b7ef432e059a63a214a8b495a8d0b4e1cae265c
joseph-sentry <136376984+joseph-sentry@users.noreply.github.com>
2024-02-01 15:43:39 -0500

    Add test results finisher (#234)
    * feat: create TestResultsNotifier class
    
    This class creates the PR comment message based on the test instances
    passed to it and posts/edits the comment on the PR.
    
    * feat(notify): don't notify if failed test instances exist
    
    The notify task will exit early now, if the latest test instances
    relevant to the notify's commit contain failures
    
    * feat: create test results finisher task
    
    This task will check if failures exist in the latest round
    of test instances for this commit. If they do it will run
    the test results notifier, else it will trigger the notify task
    to run.
    
    This task takes the same lock as the notify task so that they
    don't try writing to the PR comment at the same time.
    
    * feat: run the test results finisher after the processors
    
    The test result finisher runs after the test results processors
    are done running. This is achieved using a celery chord.
    
    * test: rename env to flags_hash in test_results_finisher
    
    * fix: address feedback; inline True, remove notify var
    
    * fix: incorporate insert_breaks feedback to simplify function
    
    * fix: address feedback
    
    this commit changes the int conversions of Outcome to
    str conversions.
    
    moves the db query for latest test instances for a given commit
    to a separate function
    
    and cleans up some unused vars
    
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- services/test_results.py
+++ services/test_results.py
@@ -3,15 +3,17 @@ from hashlib import sha256
 from typing import Mapping, Sequence
 
 from shared.torngit.exceptions import TorngitClientError
-from test_results_parser import Outcome, Testrun
+from sqlalchemy import desc
+from test_results_parser import Outcome
 
 from database.enums import ReportType
-from database.models import Commit, CommitReport, RepositoryFlag, Upload
+from database.models import Commit, CommitReport, RepositoryFlag, TestInstance, Upload
 from services.report import BaseReportService
 from services.repository import (
     fetch_and_update_pull_request_information_from_commit,
     get_repo_provider_service,
 )
+from services.urls import get_pull_url
 
 log = logging.getLogger(__name__)
 
@@ -94,3 +96,157 @@ def generate_test_id(repoid, testsuite, name, flags_hash):
             "utf-8"
         )
     ).hexdigest()
+
+
+class TestResultsNotifier:
+    def __init__(self, commit: Commit, commit_yaml, test_instances):
+        self.commit = commit
+        self.commit_yaml = commit_yaml
+        self.test_instances = test_instances
+
+    async def notify(self):
+        commit_report = self.commit.commit_report(report_type=ReportType.TEST_RESULTS)
+        if not commit_report:
+            log.warning(
+                "No test results commit report found for this commit",
+                extra=dict(
+                    commitid=self.commit.commitid,
+                    report_key=commit_report.external_id,
+                ),
+            )
+
+        repo_service = get_repo_provider_service(self.commit.repository, self.commit)
+
+        pull = await fetch_and_update_pull_request_information_from_commit(
+            repo_service, self.commit, self.commit_yaml
+        )
+        pullid = pull.database_pull.pullid
+        if pull is None:
+            log.info(
+                "Not notifying since there is no pull request associated with this commit",
+                extra=dict(
+                    commitid=self.commit.commitid,
+                    report_key=commit_report.external_id,
+                    pullid=pullid,
+                ),
+            )
+
+        pull_url = get_pull_url(pull.database_pull)
+
+        message = self.build_message(pull_url, self.test_instances)
+
+        try:
+            comment_id = pull.database_pull.commentid
+            if comment_id:
+                await repo_service.edit_comment(pullid, comment_id, message)
+            else:
+                res = await repo_service.post_comment(pullid, message)
+                pull.database_pull.commentid = res["id"]
+            return True
+        except TorngitClientError:
+            log.error(
+                "Error creating/updating PR comment",
+                extra=dict(
+                    commitid=self.commit.commitid,
+                    report_key=commit_report.external_id,
+                    pullid=pullid,
+                ),
+            )
+            return False
+
+    def build_message(self, url, test_instances):
+        message = []
+
+        message += [
+            f"##  [Codecov]({url}) Report",
+            "",
+            "**Test Failures Detected**: Due to failing tests, we cannot provide coverage reports at this time.",
+            "",
+            "### :x: Failed Test Results: ",
+        ]
+        failed_tests = 0
+        passed_tests = 0
+        skipped_tests = 0
+
+        failures = dict()
+
+        for test_instance in test_instances:
+            if test_instance.outcome == str(
+                Outcome.Failure
+            ) or test_instance.outcome == str(Outcome.Error):
+                failed_tests += 1
+                job_code = test_instance.upload.job_code
+                flag_names = sorted(test_instance.upload.flag_names)
+                suffix = ""
+                if job_code or flag_names:
+                    suffix = f"[{''.join(flag_names) or ''} {job_code or ''}]"
+                failures[
+                    f"{test_instance.test.testsuite}::{test_instance.test.name}{suffix}"
+                ] = test_instance.failure_message
+            elif test_instance.outcome == str(Outcome.Skip):
+                skipped_tests += 1
+            elif test_instance.outcome == str(Outcome.Pass):
+                passed_tests += 1
+
+        results = f"Completed {len(test_instances)} tests with **`{failed_tests} failed`**, {passed_tests} passed and {skipped_tests} skipped."
+
+        message.append(results)
+
+        details = [
+            "<details><summary>View the full list of failed tests</summary>",
+            "",
+            "| **File path** | **Failure message** |",
+            "| :-- | :-- |",
+        ]
+
+        message += details
+
+        failure_table = [
+            "| {0} | <pre>{1}</pre> |".format(
+                self.insert_breaks(failed_test_name),
+                failure_message.replace("\n", "<br>"),
+            )
+            for failed_test_name, failure_message in sorted(
+                failures.items(), key=lambda failure: failure[0]
+            )
+        ]
+
+        message += failure_table
+
+        return "\n".join(message)
+
+    def insert_breaks(self, table_value):
+        line_size = 70
+        lines = [
+            table_value[i : i + line_size]
+            for i in range(0, len(table_value), line_size)
+        ]
+        return "<br>".join(lines)
+
+
+def latest_test_instances_for_a_given_commit(db_session, commit_id):
+    """
+    This will result in a SQL query that looks something like this:
+
+    SELECT DISTINCT ON (rt.test_id) rt.id, rt.external_id, rt.created_at, rt.updated_at, rt.test_id, rt.duration_seconds, rt.outcome, rt.upload_id, rt.failure_message
+        FROM reports_testinstance rt JOIN reports_upload ru ON ru.id = rt.upload_id JOIN reports_commitreport rc ON rc.id = ru.report_id
+        WHERE rc.commit_id = <commit_id> ORDER BY rt.test_id, ru.created_at desc
+
+    The goal of this query is to return: "the latest test instance for each unique test based on upload creation time"
+
+    The `DISTINCT ON` test_id with the order by test_id, enforces that we are only fetching one test instance for each test
+
+    The ordering of the upload.create_at desc enforces that we get the latest test instance for that unique test
+    """
+    return (
+        db_session.query(TestInstance)
+        .join(Upload)
+        .join(CommitReport)
+        .filter(
+            CommitReport.commit_id == commit_id,
+        )
+        .order_by(TestInstance.test_id)
+        .order_by(desc(Upload.created_at))
+        .distinct(TestInstance.test_id)
+        .all()
+    )
---
+++ tasks/test_results_finisher.py
@@ -0,0 +1,144 @@
+import logging
+from typing import Any, Dict
+
+from shared.yaml import UserYaml
+from test_results_parser import Outcome
+
+from app import celery_app
+from database.enums import ReportType
+from database.models import Commit, CommitReport, Test, TestInstance, Upload
+from services.lock_manager import LockManager, LockRetry, LockType
+from services.test_results import (
+    TestResultsNotifier,
+    latest_test_instances_for_a_given_commit,
+)
+from tasks.base import BaseCodecovTask
+from tasks.notify import notify_task_name
+
+log = logging.getLogger(__name__)
+
+test_results_finisher_task_name = "app.tasks.test_results.TestResultsFinisherTask"
+
+
+class TestResultsFinisherTask(BaseCodecovTask, name=test_results_finisher_task_name):
+    async def run_async(
+        self,
+        db_session,
+        chord_result: Dict[str, Any],
+        *,
+        repoid: int,
+        commitid: str,
+        commit_yaml: dict,
+        **kwargs,
+    ):
+        repoid = int(repoid)
+        commit_yaml = UserYaml.from_dict(commit_yaml)
+
+        log.info(
+            "Starting test results finisher task",
+            extra=dict(
+                repoid=repoid,
+                commit=commitid,
+                commit_yaml=commit_yaml,
+            ),
+        )
+
+        lock_manager = LockManager(
+            repoid=repoid,
+            commitid=commitid,
+            report_type=ReportType.COVERAGE,
+            lock_timeout=max(80, self.hard_time_limit_task),
+        )
+
+        try:
+            # this needs to be the coverage notification lock
+            # since both tests post/edit the same comment
+            with lock_manager.locked(
+                LockType.NOTIFICATION,
+                retry_num=self.request.retries,
+            ):
+                return await self.process_async_within_lock(
+                    db_session=db_session,
+                    repoid=repoid,
+                    commitid=commitid,
+                    commit_yaml=commit_yaml,
+                    previous_result=chord_result,
+                    **kwargs,
+                )
+        except LockRetry as retry:
+            self.retry(max_retries=5, countdown=retry.countdown)
+
+    async def process_async_within_lock(
+        self,
+        *,
+        db_session,
+        repoid: int,
+        commitid: str,
+        commit_yaml: UserYaml,
+        previous_result: Dict[str, Any],
+        **kwargs,
+    ):
+        log.info(
+            "Running test results finishers",
+            extra=dict(
+                repoid=repoid,
+                commit=commitid,
+                commit_yaml=commit_yaml,
+                parent_task=self.request.parent_id,
+            ),
+        )
+
+        commit: Commit = (
+            db_session.query(Commit).filter_by(repoid=repoid, commitid=commitid).first()
+        )
+        assert commit, "commit not found"
+
+        if self.check_if_no_success(previous_result):
+            # every processor errored, nothing to notify on
+            return {"notify_attempted": False, "notify_succeeded": False}
+
+        test_instances = latest_test_instances_for_a_given_commit(
+            db_session, commit.id_
+        )
+
+        if self.check_if_no_failures(test_instances):
+            self.app.tasks[notify_task_name].apply_async(
+                args=None,
+                kwargs=dict(
+                    repoid=repoid, commitid=commitid, current_yaml=commit_yaml.to_dict()
+                ),
+            )
+            return {"notify_attempted": False, "notify_succeeded": False}
+
+        notifier = TestResultsNotifier(commit, commit_yaml, test_instances)
+        success = await notifier.notify()
+
+        log.info(
+            "Finished test results notify",
+            extra=dict(
+                repoid=repoid,
+                commit=commitid,
+                commit_yaml=commit_yaml,
+                parent_task=self.request.parent_id,
+            ),
+        )
+
+        return {"notify_attempted": True, "notify_succeeded": success}
+
+    def check_if_no_success(self, previous_result):
+        return all(
+            (
+                testrun_list["successful"] is False
+                for result in previous_result
+                for testrun_list in result
+            )
+        )
+
+    def check_if_no_failures(self, testrun_list):
+        return all(
+            [instance.outcome != str(Outcome.Failure) for instance in testrun_list]
+        )
+
+
+RegisteredTestResultsFinisherTask = celery_app.register_task(TestResultsFinisherTask())
+test_results_finisher_task = celery_app.tasks[RegisteredTestResultsFinisherTask.name]
