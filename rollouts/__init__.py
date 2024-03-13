7ea10af4c496cb28a362471d26a4f12df02634f0
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2024-03-13 17:33:45 +0100

    perf: experiment with bulk instert test results (#319)
    * perf: experiment with bulk instert test results
    
    These changes have 2 obejctives:
    1. (duh) try to speed up time of writing tests and test instances in the DB.
        This is inspired by https://docs.sqlalchemy.org/en/13/faq/performance.html#i-m-inserting-400-000-rows-with-the-orm-and-it-s-really-slow
        The idea is to bulk_update and let the DB handle the concurrency and the conflicts.
    
    2. Test using Sentry metrics + Features for perf potential improvements.
        Although I suspect the time benefit will be enough for us to prefer the bulk_insert technique I don't know what the benefit is (potentially). And there's the drawback of using extra memory, which I'm also not sure how much extra memory it is.
        So I want to use this opportunity to explore this setup of running perf experiments :D
    
    * features interface changed

--- rollouts/__init__.py
+++ rollouts/__init__.py
@@ -5,6 +5,8 @@ from database.models import Owner, Repository
 # Declare the feature variants and parameters via Django Admin
 LIST_REPOS_GENERATOR_BY_OWNER_ID = Feature("list_repos_generator")
 
+BULK_INSERT_TEST_INSTANCES = Feature("bulk_insert_test_instances")
+
 # Eventually we want all repos to use this
 # This flag will just help us with the rollout process
 USE_LABEL_INDEX_IN_REPORT_PROCESSING_BY_REPO_ID = Feature(
