5d5c979a70394f9bc5b58cc4fc7c300dc01f8a0b
trent-codecov <trent@codecov.io>
2024-05-01 15:25:21 -0400

    Fix lint

--- helpers/tests/unit/test_sentry.py
+++ helpers/tests/unit/test_sentry.py
@@ -7,7 +7,10 @@ class TestSentry(object):
     def test_initialize_sentry(self, mocker, mock_configuration):
         mock_configuration._params["services"] = {"sentry": {"server_dsn": "this_dsn"}}
         cluster = "test_env"
-        mocker.patch.dict(os.environ, {"RELEASE_VERSION": "FAKE_VERSION_FOR_YOU", "CLUSTER_ENV": cluster})
+        mocker.patch.dict(
+            os.environ,
+            {"RELEASE_VERSION": "FAKE_VERSION_FOR_YOU", "CLUSTER_ENV": cluster},
+        )
         mocked_init = mocker.patch("helpers.sentry.sentry_sdk.init")
         mocked_set_tag = mocker.patch("helpers.sentry.sentry_sdk.set_tag")
         assert initialize_sentry() is None
@@ -21,5 +24,3 @@ class TestSentry(object):
             integrations=[mocker.ANY, mocker.ANY, mocker.ANY, mocker.ANY],
         )
         mocked_set_tag.assert_called_with("cluster", cluster)
-
-    
