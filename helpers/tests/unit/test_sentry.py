2406f779ff54bdd200e87081fc7aa3382b352bea
Trent Schmidt <trent@codecov.io>
2024-05-01 15:06:30 -0400

    Update test_sentry.py

--- helpers/tests/unit/test_sentry.py
+++ helpers/tests/unit/test_sentry.py
@@ -6,8 +6,10 @@ from helpers.sentry import before_send, initialize_sentry
 class TestSentry(object):
     def test_initialize_sentry(self, mocker, mock_configuration):
         mock_configuration._params["services"] = {"sentry": {"server_dsn": "this_dsn"}}
-        mocker.patch.dict(os.environ, {"RELEASE_VERSION": "FAKE_VERSION_FOR_YOU"})
+        cluster = "test_env"
+        mocker.patch.dict(os.environ, {"RELEASE_VERSION": "FAKE_VERSION_FOR_YOU", "CLUSTER_ENV": cluster})
         mocked_init = mocker.patch("helpers.sentry.sentry_sdk.init")
+        mocked_set_tag = mocker.patch("helpers.sentry.sentry_sdk.set_tag")
         assert initialize_sentry() is None
         mocked_init.assert_called_with(
             "this_dsn",
@@ -18,3 +20,6 @@ class TestSentry(object):
             environment="production",
             integrations=[mocker.ANY, mocker.ANY, mocker.ANY, mocker.ANY],
         )
+        mocked_set_tag.assert_called_with("cluster", cluster)
+
+    
