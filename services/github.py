d1a664920f034099c751a554acabe7844c78c5ce
Gguidini <giovanni@codecov.io>
2024-05-22 19:44:52 +0200

    feat: Add functions to get/set apps to commits
    Adds `set_github_app_for_commit` and `get_github_app_for_commit` functions.
    These let us set and later get a GithubAppInstallation.id that's related to
    a Commit.id

--- services/github.py
+++ services/github.py
@@ -1,11 +1,14 @@
 import logging
 from typing import Optional
 
+from redis import RedisError
 from shared.github import InvalidInstallationError
 from shared.github import get_github_integration_token as _get_github_integration_token
 
+from database.models.core import Commit
 from helpers.cache import cache
 from helpers.exceptions import RepositoryWithoutValidBotError
+from services.redis import get_redis_connection
 
 log = logging.getLogger(__name__)
 
@@ -24,3 +27,35 @@ def get_github_integration_token(
     except InvalidInstallationError:
         log.warning("Failed to get installation token")
         raise RepositoryWithoutValidBotError()
+
+
+COMMIT_GHAPP_KEY_NAME = lambda commit_id: f"app_to_use_for_commit_{commit_id}"
+
+
+def set_github_app_for_commit(
+    installation_id: str | int | None, commit: Commit
+) -> bool:
+    if installation_id is None:
+        return False
+    redis = get_redis_connection()
+    try:
+        redis.set(
+            COMMIT_GHAPP_KEY_NAME(commit.id), str(installation_id), ex=(60 * 60 * 2)
+        )  # 2h
+        return True
+    except RedisError:
+        log.exception(
+            "Failed to set app for commit", extra=dict(commit=commit.commitid)
+        )
+        return False
+
+
+def get_github_app_for_commit(commit: Commit) -> str | None:
+    redis = get_redis_connection()
+    try:
+        return redis.get(COMMIT_GHAPP_KEY_NAME(commit.id))
+    except RedisError:
+        log.exception(
+            "Failed to get app for commit", extra=dict(commit=commit.commitid)
+        )
+        return None
