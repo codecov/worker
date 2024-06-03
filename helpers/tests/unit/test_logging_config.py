290de75151589d4b11b85b8fe00304b82e3b44ae
joseph-sentry <joseph.sawaya@sentry.io>
2024-06-03 16:29:56 -0400

    fix: get the logs working again
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- helpers/tests/unit/test_logging_config.py
+++ helpers/tests/unit/test_logging_config.py
@@ -40,11 +40,8 @@ class TestLoggingConfig(object):
         }
 
     def test_add_fields_with_task(self, mocker):
-        mock_get_task = mocker.patch(
-            "helpers.logging_config.get_current_task",
-            return_value=mocker.MagicMock(request=mocker.MagicMock(id="abcdef")),
-        )
-        mock_get_task.return_value.name = "lkjhg"
+        _ = mocker.patch("helpers.logging_config.task_name", "lkjhg")
+        _ = mocker.patch("helpers.logging_config.task_id", "abcdef")
         log_record, record, message_dict = {}, mocker.MagicMock(), {"message": "aaa"}
         log_formatter = CustomLocalJsonFormatter()
         log_formatter.add_fields(log_record, record, message_dict)
