d6d892bde35147a3939de0257059f3cb8d1a8e6c
joseph-sentry <joseph.sawaya@sentry.io>
2024-07-03 13:27:10 -0400

    fix: add logs and commit transaction manually in loop
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- tasks/backfill_test_instances.py
+++ tasks/backfill_test_instances.py
@@ -9,26 +9,34 @@ from tasks.base import BaseCodecovTask
 
 log = logging.getLogger(__name__)
 
+from django.db import transaction
+
 
 class BackfillTestInstancesTask(
     BaseCodecovTask, name=backfill_test_instances_task_name
 ):
-    def run_impl(self, *args, dry_run=True, **kwargs):
+    def run_impl(
+        self,
+        *args,
+        count=None,
+        **kwargs,
+    ):
         log.info(
             "Updating test instances",
         )
 
-        test_instance_list = (
-            TestInstance.objects.select_related("upload__report__commit")
-            .filter(
-                Q(branch__isnull=True)
-                | Q(commitid__isnull=True)
-                | Q(repoid__isnull=True)
-            )
-            .all()
-        )
+        test_instances_missing_info_count = TestInstance.objects.filter(
+            Q(branch__isnull=True) | Q(commitid__isnull=True) | Q(repoid__isnull=True)
+        ).count()
 
-        for i in range(0, test_instance_list.count(), 1000):
+        if count is None:
+            count = test_instances_missing_info_count
+        else:
+            count = min(count, test_instances_missing_info_count)
+
+        log.info("Updating {count} test instances")
+
+        for _ in range(0, test_instances_missing_info_count, 1000):
             updates = []
             test_instances_missing_info = (
                 TestInstance.objects.select_related("upload__report__commit")
@@ -39,13 +47,24 @@ class BackfillTestInstancesTask(
                 )
                 .order_by("id")[0:1000]
             )
+
+            log.info("gathered test instances missing info")
+
             for test_instance in test_instances_missing_info:
                 test_instance.branch = test_instance.upload.report.commit.branch
                 test_instance.commitid = test_instance.upload.report.commit.commitid
                 test_instance.repoid = test_instance.upload.report.commit.repository_id
                 updates.append(test_instance)
 
+            log.info("updated individual objects")
+
             TestInstance.objects.bulk_update(updates, ["branch", "commitid", "repoid"])
+
+            log.info("bulk updated objects")
+
+            transaction.commit()
+
+            log.info("commited transaction")
 
         log.info(
             "Done updating test instances",
