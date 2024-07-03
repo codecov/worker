b269e83dadd7c6ee6f0d0dc7a41d75042b4421cb
joseph-sentry <joseph.sawaya@sentry.io>
2024-07-03 16:30:42 -0400

    fix: sync pull checks flaky test feature flag

--- tasks/sync_pull.py
+++ tasks/sync_pull.py
@@ -21,7 +21,7 @@ from database.models import Commit, Pull, Repository
 from helpers.exceptions import RepositoryWithoutValidBotError
 from helpers.github_installation import get_installation_name_for_owner_for_task
 from helpers.metrics import metrics
-from rollouts import SYNC_PULL_LOCK_TIMEOUT
+from rollouts import FLAKY_TEST_DETECTION, SYNC_PULL_LOCK_TIMEOUT
 from services.comparison.changes import get_changes
 from services.redis import get_redis_connection
 from services.report import Report, ReportService
@@ -479,7 +479,11 @@ class PullSyncTask(BaseCodecovTask, name=pulls_task_name):
         self, repoid, commits_on_pr, pull_dict, current_yaml: UserYaml
     ):
         # but only if flake processing is enabled for this repo
-        if read_yaml_field(current_yaml, ("test_analytics", "flake_detection"), False):
+        if FLAKY_TEST_DETECTION.check_value(
+            identifier=repoid, default=False
+        ) and read_yaml_field(
+            current_yaml, ("test_analytics", "flake_detection"), False
+        ):
             branch = pull_dict["head"]["branch"]
             self.app.tasks[process_flakes_task_name].apply_async(
                 kwargs=dict(
