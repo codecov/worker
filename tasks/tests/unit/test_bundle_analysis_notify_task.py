e9840699857f38c04082fad414abd3f6cca354b6
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2024-08-14 21:24:46 +1000

    fix: UnboundLocalError (#617)

--- tasks/tests/unit/test_bundle_analysis_notify_task.py
+++ tasks/tests/unit/test_bundle_analysis_notify_task.py
@@ -38,3 +38,20 @@ def test_bundle_analysis_notify_task(
         "notify_attempted": True,
         "notify_succeeded": NotificationSuccess.FULL_SUCCESS,
     }
+
+
+def test_bundle_analysis_notify_skips_if_all_processing_fail(dbsession):
+    commit = CommitFactory.create()
+    dbsession.add(commit)
+    dbsession.flush()
+    result = BundleAnalysisNotifyTask().run_impl(
+        dbsession,
+        {"results": [{"error": True}]},
+        repoid=commit.repoid,
+        commitid=commit.commitid,
+        commit_yaml={},
+    )
+    assert result == {
+        "notify_attempted": False,
+        "notify_succeeded": NotificationSuccess.ALL_ERRORED,
+    }
