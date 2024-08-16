c08ee10d6c5e6944d76ff9997a8590d3843f384f
Arpad Borsos <arpad.borsos@sentry.io>
2024-08-16 14:30:19 +0200

    Consider contents when choosing between v1/legacy ReportParser

--- services/report/parser/__init__.py
+++ services/report/parser/__init__.py
@@ -1,9 +1,18 @@
+import sentry_sdk
+
 from database.models.reports import Upload
 from services.report.parser.legacy import LegacyReportParser
 from services.report.parser.version_one import VersionOneReportParser
 
 
-def get_proper_parser(upload: Upload):
+def get_proper_parser(upload: Upload, contents: bytes):
     if upload.upload_extras and upload.upload_extras.get("format_version") == "v1":
-        return VersionOneReportParser()
+        contents = contents.strip()
+        if contents.startswith(b"{") and contents.endswith(b"}"):
+            return VersionOneReportParser()
+        else:
+            with sentry_sdk.configure_scope() as scope:
+                scope.set_extra("upload_extras", upload.upload_extras)
+                scope.set_extra("contents", contents[:64])
+                sentry_sdk.capture_message("Upload `format_version` lied to us")
     return LegacyReportParser()
