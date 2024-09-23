9898109d4ab8d28c92f1418501dcc18a39974c92
joseph-sentry <joseph.sawaya@sentry.io>
2024-09-23 13:53:03 -0400

    feat: add backfill one off scripts
    these are one off python scripts to be run directly that will
    modify the database
    
    the first backfills the daily test rollups starting on a given day,
    and ending on a given date, starting for a given repo (it will
    backfill in repo id order)
    
    the second backfills the TestFlagBridge objects for all repos with the
    option to focus on a given repo, specified by repoid

---
+++ one_off_scripts/backfill_test_flag_bridges.py
@@ -0,0 +1,49 @@
+import logging
+
+from django.db import transaction as django_transaction
+from shared.django_apps.core.models import Repository
+from shared.django_apps.reports.models import (
+    RepositoryFlag,
+    Test,
+    TestFlagBridge,
+    TestInstance,
+)
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
+
+
+def backfill_test_flag_bridges(repoid=None):
+    repos = Repository.objects.filter(test_analytics_enabled=True)
+    if repoid is not None:
+        repos = repos.filter(repoid=repoid)
+
+    for repo in repos:
+        tests = Test.objects.filter(repository_id=repo.repoid)
+
+        flags = {
+            flag.flag_name: flag
+            for flag in RepositoryFlag.objects.filter(repository=repo)
+        }
+
+        for test in tests:
+            TestFlagBridge.objects.filter(test == test).delete()
+
+            first_test_instance = TestInstance.objects.filter(
+                test_id=test.id
+            ).select_related("upload")[:1][0]
+
+            if first_test_instance is None:
+                continue
+
+            flag_names = first_test_instance.upload.flag_names
+
+            for flag_name in flag_names:
+                new_bridge = TestFlagBridge(test=test, flag=flags[flag_name])
+                new_bridge.save()
+
+        log.info(
+            "Done creating flag bridges for repo",
+            extra=dict(repoid=repoid, num_tests=len(tests)),
+        )
+        django_transaction.commit()
