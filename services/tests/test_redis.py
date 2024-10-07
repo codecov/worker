f83c94ad3efe960af066a13a97dc1934755a7d86
Arpad Borsos <arpad.borsos@sentry.io>
2024-10-07 15:29:33 +0200

    fix test, wtf

--- services/tests/test_redis.py
+++ services/tests/test_redis.py
@@ -1,10 +1,8 @@
 from services.redis import get_redis_connection
-from test_utils.base import BaseTestCase
 
 
-class TestRedis(BaseTestCase):
-    def test_get_redis_connection(self, mocker, mock_configuration):
-        mocked = mocker.patch("services.redis.Redis.from_url")
-        res = get_redis_connection()
-        assert res is not None
-        mocked.assert_called_with("redis://redis:@localhost:6379/")
+def test_get_redis_connection(mocker):
+    mocked = mocker.patch("services.redis.Redis.from_url")
+    res = get_redis_connection()
+    assert res is not None
+    mocked.assert_called_with("redis://redis:6379")
