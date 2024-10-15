e373a034feccd1d2fef22b0bed7d530d642bf128
Arpad Borsos <arpad.borsos@sentry.io>
2024-10-15 10:28:21 +0200

    Move loading and merging intermediate Reports to its own file (#767)

---
+++ services/processing/loading.py
@@ -0,0 +1,102 @@
+import json
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
+
+import sentry_sdk
+from shared.reports.editable import EditableReport
+
+from services.archive import ArchiveService, MinioEndpoints
+
+
+@dataclass
+class IntermediateReport:
+    upload_id: int
+    """
+    The `Upload` id for which this report was loaded.
+    """
+
+    report: EditableReport
+    """
+    The loaded Report.
+    """
+
+
+@sentry_sdk.trace
+def load_intermediate_reports(
+    archive_service: ArchiveService,
+    commitsha: str,
+    upload_ids: list[int],
+) -> list[IntermediateReport]:
+    @sentry_sdk.trace
+    def load_report(upload_id: int) -> IntermediateReport:
+        repo_hash = archive_service.storage_hash
+        json_path, chunks_path = intermediate_report_paths(
+            repo_hash, commitsha, upload_id
+        )
+
+        chunks = archive_service.read_file(chunks_path).decode(errors="replace")
+        report_json = json.loads(archive_service.read_file(json_path))
+
+        report = EditableReport.from_chunks(
+            chunks=chunks,
+            files=report_json["files"],
+            sessions=report_json["sessions"],
+            totals=report_json.get("totals"),
+        )
+        return IntermediateReport(upload_id, report)
+
+    def instrumented_load_report(upload_id: int) -> IntermediateReport:
+        with sentry_sdk.isolation_scope() as _scope:
+            return load_report(upload_id)
+
+    with ThreadPoolExecutor() as pool:
+        loaded_reports = pool.map(instrumented_load_report, upload_ids)
+        return list(loaded_reports)
+
+
+@sentry_sdk.trace
+def cleanup_intermediate_reports(
+    archive_service: ArchiveService,
+    commitsha: str,
+    upload_ids: list[int],
+):
+    """
+    Cleans up the files in storage that contain the "intermediate Report"s
+    from parallel processing, as well as the copy of the "master Report" used
+    for the "experiment" mode.
+    """
+    repo_hash = archive_service.storage_hash
+
+    # there are only relevant for the "experiment" mode:
+    files_to_delete = list(experiment_report_paths(repo_hash, commitsha))
+
+    for upload_id in upload_ids:
+        files_to_delete.extend(
+            intermediate_report_paths(repo_hash, commitsha, upload_id)
+        )
+
+    archive_service.delete_files(files_to_delete)
+
+
+def intermediate_report_paths(
+    repo_hash: str, commitsha: str, upload_id: int
+) -> tuple[str, str]:
+    # TODO: migrate these files to a better storage location
+    prefix = f"v4/repos/{repo_hash}/commits/{commitsha}/parallel/incremental"
+    chunks_path = f"{prefix}/chunk{upload_id}.txt"
+    json_path = f"{prefix}/files_and_sessions{upload_id}.txt"
+    return json_path, chunks_path
+
+
+def experiment_report_paths(repo_hash: str, commitsha: str) -> tuple[str, str]:
+    return MinioEndpoints.parallel_upload_experiment.get_path(
+        version="v4",
+        repo_hash=repo_hash,
+        commitid=commitsha,
+        file_name="files_and_sessions",
+    ), MinioEndpoints.parallel_upload_experiment.get_path(
+        version="v4",
+        repo_hash=repo_hash,
+        commitid=commitsha,
+        file_name="chunks",
+    )
--- services/tests/test_processing_state.py
+++ services/tests/test_processing_state.py
@@ -46,26 +46,26 @@ def test_concurrent_uploads():
 def test_batch_merging_many_uploads():
     state = ProcessingState(1234, uuid4().hex)
 
-    state.mark_uploads_as_processing([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    state.mark_uploads_as_processing([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
-    for id in range(1, 9):
+    for id in range(1, 12):
         state.mark_upload_as_processed(id)
 
     # we have only processed 8 out of 9. we want to do a batched merge
     assert should_perform_merge(state.get_upload_numbers())
     merging = state.get_uploads_for_merging()
-    assert len(merging) == 5  # = MERGE_BATCH_SIZE
+    assert len(merging) == 10  # = MERGE_BATCH_SIZE
     state.mark_uploads_as_merged(merging)
 
     # but no notifications yet
     assert not should_trigger_postprocessing(state.get_upload_numbers())
 
-    state.mark_upload_as_processed(9)
+    state.mark_upload_as_processed(12)
 
     # with the last upload being processed, we do another merge, and then trigger notifications
     assert should_perform_merge(state.get_upload_numbers())
     merging = state.get_uploads_for_merging()
-    assert len(merging) == 4
+    assert len(merging) == 2
     state.mark_uploads_as_merged(merging)
 
     assert should_trigger_postprocessing(state.get_upload_numbers())
