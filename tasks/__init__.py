ebba1420dc2fb2cc0a481d27d530d537a7fe1c72
Arpad Borsos <arpad.borsos@sentry.io>
2024-11-07 16:16:07 +0100

    Remove `CleanLabelsIndex` task
    According to metrics, this task was never actually executed, so lets remove it.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -59,6 +59,5 @@ from tasks.trial_expiration import trial_expiration_task
 from tasks.trial_expiration_cron import trial_expiration_cron_task
 from tasks.update_branches import update_branches_task_name
 from tasks.upload import upload_task
-from tasks.upload_clean_labels_index import clean_labels_index_task
 from tasks.upload_finisher import upload_finisher_task
 from tasks.upload_processor import upload_processor_task
