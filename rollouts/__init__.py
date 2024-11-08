532d6ed797673b2a1a440ed5a2964c88adce0311
Arpad Borsos <arpad.borsos@sentry.io>
2024-11-08 12:21:29 +0100

    Hardcode and cleanup `intermediate_reports_in_redis` feature
    The feature has been fully rolled out by now, so it is time to clean up the transitional code.

--- rollouts/__init__.py
+++ rollouts/__init__.py
@@ -4,8 +4,6 @@ from shared.rollouts import Feature
 FLAKY_TEST_DETECTION = Feature("flaky_test_detection")
 FLAKY_SHADOW_MODE = Feature("flaky_shadow_mode")
 
-INTERMEDIATE_REPORTS_IN_REDIS = Feature("intermediate_reports_in_redis")
-
 CARRYFORWARD_BASE_SEARCH_RANGE_BY_OWNER = Feature("carryforward_base_search_range")
 
 SYNC_PULL_USE_MERGE_COMMIT_SHA = Feature("sync_pull_use_merge_commit_sha")
