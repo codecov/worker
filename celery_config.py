8b57acf4b8c539b0cdf7b0b0ebd4593b51611fa9
Nora Shapiro <nora.shapiro@sentry.io>
2024-12-05 23:31:11 -0800

    add FlareCleanupTask

--- celery_config.py
+++ celery_config.py
@@ -10,6 +10,7 @@ from celery.schedules import crontab
 from shared.celery_config import (
     BaseCeleryConfig,
     brolly_stats_rollup_task_name,
+    flare_cleanup_task_name,
     gh_app_webhook_check_task_name,
     health_check_task_name,
     profiling_finding_task_name,
@@ -90,6 +91,13 @@ def _beat_schedule():
             "task": trial_expiration_cron_task_name,
             # 4 UTC is 12am EDT
             "schedule": crontab(minute="0", hour="4"),
+            "kwargs": {
+                "cron_task_generation_time_iso": BeatLazyFunc(get_utc_now_as_iso_format)
+            },
+        },
+        "flare_cleanup": {
+            "task": flare_cleanup_task_name,
+            "schedule": crontab(minute="0", hour="4"),  # every day, 4am UTC (8pm PT)
             "kwargs": {
                 "cron_task_generation_time_iso": BeatLazyFunc(get_utc_now_as_iso_format)
             },
