ce7dd096c3401683821c3056c9be10205aae4c09
joseph-sentry <joseph.sawaya@sentry.io>
2024-12-23 16:39:48 -0500

    feat: use new test results notifier in finisher
    we want to fetch any relevant upload errors and pass them to the test
    results notifier in the finisher so it can display them

--- tasks/tests/unit/test_ta_finisher_task.py
+++ tasks/tests/unit/test_ta_finisher_task.py
@@ -6,15 +6,92 @@ from typing import Any
 from unittest.mock import AsyncMock
 
 import msgpack
+import pytest
 
-from database.models import DailyTestRollup, Test, TestInstance
-from database.tests.factories import UploadFactory
+from database.models import Commit, DailyTestRollup, Test, TestInstance, Upload
+from database.tests.factories import CommitFactory, UploadFactory
 from services.redis import get_redis_connection
 from services.urls import services_short_dict
 from tasks.ta_finisher import TAFinisherTask
 from tasks.ta_processor import TAProcessorTask
 
 here = Path(__file__)
+
+
+upload_idx = 0
+
+
+@pytest.fixture
+def generate_junit(mock_storage, dbsession):
+    def _generate_junit(
+        testruns: list[dict[str, Any]] | None = None,
+        testsuite_name: str = "hello_world",
+        raw: str | None = None,
+        commit: Commit | None = None,
+    ) -> Upload:
+        assert raw or testruns
+
+        global upload_idx
+        url = f"literally/whatever/{upload_idx}"
+        upload_idx += 1
+
+        if raw is None:
+            if not testruns:
+                raise ValueError("generate_junit fixture: testruns must be provided")
+            content = generate_junit_xml(testruns, testsuite_name)
+        else:
+            content = raw
+
+        json_content: dict[str, Any] = {
+            "test_results_files": [
+                {
+                    "filename": "hello_world.junit.xml",
+                    "data": base64.b64encode(zlib.compress(content.encode())).decode(),
+                }
+            ],
+        }
+
+        mock_storage.write_file("archive", url, json.dumps(json_content).encode())
+        upload = UploadFactory.create(storage_path=url)
+        if commit:
+            upload.report.commit = commit
+        dbsession.add(upload)
+        dbsession.flush()
+        upload.report.commit.branch = "main"
+        upload.report.report_type = "test_results"
+        dbsession.flush()
+
+        return upload
+
+    return _generate_junit
+
+
+@pytest.fixture
+def mock_repo_provider_service(mocker):
+    mock_repo_provider_service = AsyncMock()
+    mocker.patch(
+        "tasks.ta_finisher.get_repo_provider_service",
+        return_value=mock_repo_provider_service,
+    )
+    mocker.patch(
+        "helpers.notifier.get_repo_provider_service",
+        return_value=mock_repo_provider_service,
+    )
+    return mock_repo_provider_service
+
+
+@pytest.fixture
+def mock_pull_request_information(mocker):
+    mock_pull_request_information = AsyncMock()
+    mocker.patch(
+        "tasks.ta_finisher.fetch_and_update_pull_request_information_from_commit",
+        return_value=mock_pull_request_information,
+    )
+    mocker.patch(
+        "helpers.notifier.fetch_and_update_pull_request_information_from_commit",
+        return_value=mock_pull_request_information,
+    )
+    return mock_pull_request_information
 
 
 def generate_junit_xml(
@@ -68,9 +145,15 @@ def generate_junit_xml(
     return "\n".join(header)
 
 
-def test_test_analytics(dbsession, mocker, mock_storage, celery_app):
-    url = "literally/whatever"
-
+def test_test_analytics(
+    dbsession,
+    mocker,
+    mock_storage,
+    celery_app,
+    mock_repo_provider_service,
+    mock_pull_request_information,
+    generate_junit,
+):
     testruns = [
         {
             "name": "test_divide",
@@ -83,24 +166,9 @@ def test_test_analytics(dbsession, mocker, mock_storage, celery_app):
         {"name": "test_subtract", "outcome": "error", "duration_seconds": 0.004},
     ]
 
-    content: str = generate_junit_xml(testruns)
-    json_content: dict[str, Any] = {
-        "test_results_files": [
-            {
-                "filename": "hello_world.junit.xml",
-                "data": base64.b64encode(zlib.compress(content.encode())).decode(),
-            }
-        ],
-    }
-    mock_storage.write_file("archive", url, json.dumps(json_content).encode())
-    upload = UploadFactory.create(storage_path=url)
-    dbsession.add(upload)
-    dbsession.flush()
-    upload.report.commit.branch = "main"
-    upload.report.report_type = "test_results"
-    dbsession.flush()
+    upload = generate_junit(testruns)
 
-    argument = {"url": url, "upload_id": upload.id_}
+    argument = {"url": upload.storage_path, "upload_id": upload.id_}
 
     mocker.patch.object(TAProcessorTask, "app", celery_app)
     mocker.patch.object(TAFinisherTask, "app", celery_app)
@@ -109,17 +177,6 @@ def test_test_analytics(dbsession, mocker, mock_storage, celery_app):
         "app.tasks.flakes.ProcessFlakesTask": mocker.MagicMock(),
         "app.tasks.cache_rollup.CacheTestRollupsTask": mocker.MagicMock(),
     }
-
-    mock_repo_provider_service = AsyncMock()
-    mocker.patch(
-        "tasks.ta_finisher.get_repo_provider_service",
-        return_value=mock_repo_provider_service,
-    )
-    mock_pull_request_information = AsyncMock()
-    mocker.patch(
-        "tasks.ta_finisher.fetch_and_update_pull_request_information_from_commit",
-        return_value=mock_pull_request_information,
-    )
 
     result = TAProcessorTask().run_impl(
         dbsession,
@@ -275,3 +332,190 @@ To view more test analytics, go to the [Test Analytics Dashboard](https://app.co
         0.003,
         0.004,
     ]
+
+
+def test_test_analytics_error_comment(
+    dbsession,
+    mocker,
+    mock_storage,
+    celery_app,
+    mock_repo_provider_service,
+    mock_pull_request_information,
+    generate_junit,
+):
+    wrong_xml = """<?xml version="1.0" encoding="utf-8"?>
+<testsuites>
+    <testsuite>
+        <testcase time="0.001">
+            <failure message="hello world"/>
+        </testcase>
+    </testsuite>
+</testsuites>
+"""
+    upload = generate_junit(raw=wrong_xml)
+
+    argument = {"url": upload.storage_path, "upload_id": upload.id_}
+
+    mocker.patch.object(TAProcessorTask, "app", celery_app)
+    mocker.patch.object(TAFinisherTask, "app", celery_app)
+
+    result = TAProcessorTask().run_impl(
+        dbsession,
+        repoid=upload.report.commit.repoid,
+        commitid=upload.report.commit.commitid,
+        commit_yaml={"codecov": {"max_report_age": False}},
+        argument=argument,
+    )
+
+    assert result is False
+
+    result = TAFinisherTask().run_impl(
+        dbsession,
+        chord_result=[result],
+        repoid=upload.report.commit.repoid,
+        commitid=upload.report.commit.commitid,
+        commit_yaml={"codecov": {"max_report_age": False}},
+    )
+
+    assert result["notify_attempted"] is False
+    assert result["notify_succeeded"] is False
+    assert result["queue_notify"] is True
+
+    mock_repo_provider_service.edit_comment.assert_called_once()
+    mock_repo_provider_service.edit_comment.assert_called_once_with(
+        mock_pull_request_information.database_pull.pullid,
+        mock_pull_request_information.database_pull.commentid,
+        """### :x: Unsupported file format
+
+> Upload processing failed due to unsupported file format. Please review the parser error message:
+> `Error parsing JUnit XML in hello_world.junit.xml at 4:32: ParserError: No name found`
+> For more help, visit our [troubleshooting guide](https://docs.codecov.com/docs/test-analytics#troubleshooting).
+""",
+    )
+
+    tests = dbsession.query(Test).all()
+    assert len(tests) == 0
+
+    test_instances = dbsession.query(TestInstance).all()
+    assert len(test_instances) == 0
+
+    rollups = dbsession.query(DailyTestRollup).all()
+    assert len(rollups) == 0
+
+
+def test_test_analytics_regular_comment_with_error(
+    dbsession,
+    mocker,
+    mock_storage,
+    celery_app,
+    mock_repo_provider_service,
+    mock_pull_request_information,
+    generate_junit,
+):
+    mocker.patch.object(TAProcessorTask, "app", celery_app)
+    mocker.patch.object(TAFinisherTask, "app", celery_app)
+
+    commit = CommitFactory.create()
+
+    wrong_xml = """<?xml version="1.0" encoding="utf-8"?>
+<testsuites>
+    <testsuite>
+        <testcase time="0.001">
+            <failure message="hello world"/>
+        </testcase>
+    </testsuite>
+</testsuites>
+"""
+
+    upload = generate_junit(raw=wrong_xml, commit=commit)
+    argument = {"url": upload.storage_path, "upload_id": upload.id_}
+    first_result = TAProcessorTask().run_impl(
+        dbsession,
+        repoid=upload.report.commit.repoid,
+        commitid=upload.report.commit.commitid,
+        commit_yaml={"codecov": {"max_report_age": False}},
+        argument=argument,
+    )
+    assert first_result is False
+
+    testruns = [
+        {
+            "name": "test_divide",
+            "outcome": "fail",
+            "duration_seconds": 0.001,
+            "failure_message": "hello world",
+        },
+    ]
+
+    upload = generate_junit(testruns, commit=commit)
+    argument = {"url": upload.storage_path, "upload_id": upload.id_}
+    second_result = TAProcessorTask().run_impl(
+        dbsession,
+        repoid=upload.report.commit.repoid,
+        commitid=upload.report.commit.commitid,
+        commit_yaml={"codecov": {"max_report_age": False}},
+        argument=argument,
+    )
+    assert second_result is True
+
+    result = TAFinisherTask().run_impl(
+        dbsession,
+        chord_result=[first_result, second_result],
+        repoid=upload.report.commit.repoid,
+        commitid=upload.report.commit.commitid,
+        commit_yaml={"codecov": {"max_report_age": False}},
+    )
+
+    assert result["notify_attempted"] is True
+    assert result["notify_succeeded"] is True
+    assert result["queue_notify"] is False
+
+    short_form_service_name = services_short_dict.get(
+        upload.report.commit.repository.owner.service
+    )
+
+    mock_repo_provider_service.edit_comment.assert_called_once()
+    mock_repo_provider_service.edit_comment.assert_called_once_with(
+        mock_pull_request_information.database_pull.pullid,
+        mock_pull_request_information.database_pull.commentid,
+        f"""### :x: Unsupported file format
+
+> Upload processing failed due to unsupported file format. Please review the parser error message:
+> `Error parsing JUnit XML in hello_world.junit.xml at 4:32: ParserError: No name found`
+> For more help, visit our [troubleshooting guide](https://docs.codecov.com/docs/test-analytics#troubleshooting).
+
+---
+### :x: 1 Tests Failed:
+| Tests completed | Failed | Passed | Skipped |
+|---|---|---|---|
+| 1 | 1 | 0 | 0 |
+<details><summary>View the top 1 failed tests by shortest run time</summary>
+
+> 
+> ```python
+> tests.test_parsers.TestParsers test_divide
+> ```
+> 
+> <details><summary>Stack Traces | 0.001s run time</summary>
+> 
+> > 
+> > ```python
+> > hello world
+> > ```
+> 
+> </details>
+
+</details>
+
+To view more test analytics, go to the [Test Analytics Dashboard](https://app.codecov.io/{short_form_service_name}/{upload.report.commit.repository.owner.username}/{upload.report.commit.repository.name}/tests/{upload.report.commit.branch})
+:loudspeaker:  Thoughts on this report? [Let us know!](https://github.com/codecov/feedback/issues/304)""",
+    )
+
+    tests = dbsession.query(Test).all()
+    assert len(tests) == 1
+
+    test_instances = dbsession.query(TestInstance).all()
+    assert len(test_instances) == 1
+
+    rollups = dbsession.query(DailyTestRollup).all()
+    assert len(rollups) == 1
