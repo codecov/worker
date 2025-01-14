4c8009103187913f4ac52266eab74b0545114e75
Joseph Sawaya <joseph.sawaya@sentry.io>
2025-01-14 16:30:26 -0500

    feat: improve ta_storage.bigquery
    - generalize the way bigquery accepts query parameters
    - change type of params arg in bigquery_service to sequence
    - feat: add upload_id field to ta_testrun protobuf
    - add flags_hash field to ta_testrun protobuf
    - create new testid generation function
    - add test_id to ta_testrun proto
    - add flaky_failure to testrun protobuf
    - handle flaky failures in ta_storage.bq
    - create sql queries for reading from bq
    - write tests for ta_storage.bq aggregate queries

--- generated_proto/testrun/ta_testrun_pb2.py
+++ generated_proto/testrun/ta_testrun_pb2.py
@@ -4,32 +4,35 @@
 # source: ta_testrun.proto
 # Protobuf Python Version: 5.29.2
 """Generated protocol buffer code."""
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-
 _runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC, 5, 29, 2, "", "ta_testrun.proto"
+    _runtime_version.Domain.PUBLIC,
+    5,
+    29,
+    2,
+    '',
+    'ta_testrun.proto'
 )
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x10ta_testrun.proto"\xda\x02\n\x07TestRun\x12\x11\n\ttimestamp\x18\x01 \x01(\x03\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x11\n\tclassname\x18\x03 \x01(\t\x12\x11\n\ttestsuite\x18\x04 \x01(\t\x12\x15\n\rcomputed_name\x18\x05 \x01(\t\x12!\n\x07outcome\x18\x06 \x01(\x0e\x32\x10.TestRun.Outcome\x12\x17\n\x0f\x66\x61ilure_message\x18\x07 \x01(\t\x12\x18\n\x10\x64uration_seconds\x18\x08 \x01(\x02\x12\x0e\n\x06repoid\x18\n \x01(\x03\x12\x12\n\ncommit_sha\x18\x0b \x01(\t\x12\x13\n\x0b\x62ranch_name\x18\x0c \x01(\t\x12\r\n\x05\x66lags\x18\r \x03(\t\x12\x10\n\x08\x66ilename\x18\x0e \x01(\t\x12\x11\n\tframework\x18\x0f \x01(\t".\n\x07Outcome\x12\n\n\x06PASSED\x10\x00\x12\n\n\x06\x46\x41ILED\x10\x01\x12\x0b\n\x07SKIPPED\x10\x02'
-)
+
+
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10ta_testrun.proto\"\xa4\x03\n\x07TestRun\x12\x11\n\ttimestamp\x18\x01 \x01(\x03\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x11\n\tclassname\x18\x03 \x01(\t\x12\x11\n\ttestsuite\x18\x04 \x01(\t\x12\x15\n\rcomputed_name\x18\x05 \x01(\t\x12!\n\x07outcome\x18\x06 \x01(\x0e\x32\x10.TestRun.Outcome\x12\x17\n\x0f\x66\x61ilure_message\x18\x07 \x01(\t\x12\x18\n\x10\x64uration_seconds\x18\x08 \x01(\x02\x12\x0e\n\x06repoid\x18\n \x01(\x03\x12\x12\n\ncommit_sha\x18\x0b \x01(\t\x12\x13\n\x0b\x62ranch_name\x18\x0c \x01(\t\x12\r\n\x05\x66lags\x18\r \x03(\t\x12\x10\n\x08\x66ilename\x18\x0e \x01(\t\x12\x11\n\tframework\x18\x0f \x01(\t\x12\x11\n\tupload_id\x18\x10 \x01(\x03\x12\x12\n\nflags_hash\x18\x11 \x01(\x0c\x12\x0f\n\x07test_id\x18\x12 \x01(\x0c\"@\n\x07Outcome\x12\n\n\x06PASSED\x10\x00\x12\n\n\x06\x46\x41ILED\x10\x01\x12\x0b\n\x07SKIPPED\x10\x02\x12\x10\n\x0c\x46LAKY_FAILED\x10\x03')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "ta_testrun_pb2", _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'ta_testrun_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
-    DESCRIPTOR._loaded_options = None
-    _globals["_TESTRUN"]._serialized_start = 21
-    _globals["_TESTRUN"]._serialized_end = 367
-    _globals["_TESTRUN_OUTCOME"]._serialized_start = 321
-    _globals["_TESTRUN_OUTCOME"]._serialized_end = 367
+  DESCRIPTOR._loaded_options = None
+  _globals['_TESTRUN']._serialized_start=21
+  _globals['_TESTRUN']._serialized_end=441
+  _globals['_TESTRUN_OUTCOME']._serialized_start=377
+  _globals['_TESTRUN_OUTCOME']._serialized_end=441
 # @@protoc_insertion_point(module_scope)
---
+++ ta_storage/utils.py
@@ -0,0 +1,28 @@
+from sys import byteorder
+
+import mmh3
+import sentry_sdk
+
+
+def calc_test_id(name: str, classname: str, testsuite: str) -> bytes:
+    h = mmh3.mmh3_x64_128()  # assumes we're running on x64 machines
+    h.update(testsuite.encode("utf-8"))
+    h.update(classname.encode("utf-8"))
+    h.update(name.encode("utf-8"))
+    test_id_hash = h.digest()
+
+    return test_id_hash
+
+
+def calc_flags_hash(flags: list[str]) -> bytes | None:
+    flags_str = " ".join(flags)  # we know that flags cannot contain spaces
+
+    # returns a tuple of two int64 values
+    # we only need the first one
+    flags_hash, _ = mmh3.hash64(flags_str)
+    try:
+        flags_hash_bytes = flags_hash.to_bytes(8, byteorder)
+        return flags_hash_bytes
+    except OverflowError as e:  # this should never happen because hash64 should always return 2 64 bit ints
+        sentry_sdk.capture_exception(e)
+        return None
