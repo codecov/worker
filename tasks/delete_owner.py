f4cef03fae033cdab4623d0aded5661183d12187
Arpad Borsos <arpad.borsos@sentry.io>
2025-01-23 12:20:07 +0100

    Fix problems discovered in Repository Deletion
    - Deleting non-existing files can raise, so catch and ignore such errors
    - Switch "Shadow Owner" to a `raw_delete` to avoid expensive django `SET NULL` / `CASCADE` queries
    - Use a `SELECT FOR UPDATE NOWAIT` when starting deletions to make this more resilient to conflicting tasks:
      - This also catches "not found" when the repo does not exist, and "integrity" errors on duplicate shadow owner inserts.
    - Last but not least, this makes the task explicitly retry on timeouts.

--- tasks/delete_owner.py
+++ tasks/delete_owner.py
@@ -1,5 +1,6 @@
 import logging
 
+from celery.exceptions import SoftTimeLimitExceeded
 from shared.celery_config import delete_owner_task_name
 
 from app import celery_app
@@ -15,7 +16,10 @@ class DeleteOwnerTask(BaseCodecovTask, name=delete_owner_task_name):
     max_retries = None  # aka, no limit on retries
 
     def run_impl(self, _db_session, ownerid: int) -> CleanupSummary:
-        return cleanup_owner(ownerid)
+        try:
+            return cleanup_owner(ownerid)
+        except SoftTimeLimitExceeded:
+            raise self.retry()
 
 
 RegisteredDeleteOwnerTask = celery_app.register_task(DeleteOwnerTask())
