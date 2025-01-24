30b426f3398c3dbe7dcdf8ce659de5bf36c253b7
Joseph Sawaya <joseph.sawaya@sentry.io>
2025-01-24 16:53:53 -0500

    address feedback i missed

--- ta_storage/utils.py
+++ ta_storage/utils.py
@@ -1,5 +1,4 @@
 import mmh3
-import sentry_sdk
 
 
 def calc_test_id(name: str, classname: str, testsuite: str) -> bytes:
@@ -18,9 +17,5 @@ def calc_flags_hash(flags: list[str]) -> bytes | None:
     # returns a tuple of two int64 values
     # we only need the first one
     flags_hash, _ = mmh3.hash64(flags_str, signed=False)
-    try:
-        flags_hash_bytes = flags_hash.to_bytes(8)
-        return flags_hash_bytes
-    except OverflowError as e:  # this should never happen because hash64 should always return 2 64 bit ints
-        sentry_sdk.capture_exception(e)
-        return None
+    flags_hash_bytes = flags_hash.to_bytes(8)
+    return flags_hash_bytes
