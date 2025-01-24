45f646381e94444bf9b2c21fdf9853c4b6162727
Joseph Sawaya <joseph.sawaya@sentry.io>
2025-01-24 16:10:12 -0500

    fix: address feedback

--- ta_storage/utils.py
+++ ta_storage/utils.py
@@ -1,5 +1,3 @@
-from sys import byteorder
-
 import mmh3
 import sentry_sdk
 
@@ -15,13 +13,13 @@ def calc_test_id(name: str, classname: str, testsuite: str) -> bytes:
 
 
 def calc_flags_hash(flags: list[str]) -> bytes | None:
-    flags_str = " ".join(flags)  # we know that flags cannot contain spaces
+    flags_str = " ".join(sorted(flags))  # we know that flags cannot contain spaces
 
     # returns a tuple of two int64 values
     # we only need the first one
-    flags_hash, _ = mmh3.hash64(flags_str)
+    flags_hash, _ = mmh3.hash64(flags_str, signed=False)
     try:
-        flags_hash_bytes = flags_hash.to_bytes(8, byteorder)
+        flags_hash_bytes = flags_hash.to_bytes(8)
         return flags_hash_bytes
     except OverflowError as e:  # this should never happen because hash64 should always return 2 64 bit ints
         sentry_sdk.capture_exception(e)
