630273066aac15c1509dda15a654bb6dbb5b8e51
joseph-sentry <136376984+joseph-sentry@users.noreply.github.com>
2025-01-30 15:12:49 -0500

    make sure to import new task in tasks/__init__.py (#1050)

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -21,6 +21,7 @@ from tasks.cache_test_rollups import cache_test_rollups_task
 from tasks.cache_test_rollups_redis import cache_test_rollups_redis_task
 from tasks.commit_update import commit_update_task
 from tasks.compute_comparison import compute_comparison_task
+from tasks.compute_component_comparison import compute_component_comparison_task
 from tasks.delete_owner import delete_owner_task
 from tasks.flush_repo import flush_repo
 from tasks.github_app_webhooks_check import gh_webhook_check_task
