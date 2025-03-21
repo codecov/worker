725a8d1a06c26d5f379f8538d5eeb4551cf33676
joseph-sentry <136376984+joseph-sentry@users.noreply.github.com>
2025-03-21 16:43:37 -0400

    fix: update migrate-timeseries.sh (#1167)

---
+++ migrate_timeseries.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+import django
+from django.core.management import call_command
+
+# Setup Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_scaffold.settings")
+django.setup()
+
+from django.conf import settings  # noqa: E402
+
+logger = logging.getLogger(__name__)
+
+
+def run_migrate_commands():
+    try:
+        if settings.TA_TIMESERIES_ENABLED:
+            logger.info("Running ta_timeseries migrations")
+            call_command(
+                "migrate",
+                database="ta_timeseries",
+                app_label="ta_timeseries",
+                settings="django_scaffold.settings",
+                verbosity=1,
+            )
+        else:
+            logger.info("Skipping ta_timeseries migrations")
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    run_migrate_commands()
