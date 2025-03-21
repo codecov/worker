c4b75e3864fa7d61a14feb4ce28c4c6553888a53
Arpad Borsos <arpad.borsos@sentry.io>
2025-03-21 11:31:12 +0100

    Remove `get_changes_using_rust` usage
    As the `rust_report` is hard-enabled because `get_impacted_files` depends on it, we are also unconditionally running both implementations of `get_changes`.
    
    But those two implementations differ in some details, and we are spamming logs with a ton of differences that noone ever looks ate to eventually move the implementation one way or the other.
    
    So lets just remove this comparison code for a tiny speedup.

--- services/notification/tests/unit/test_comparison.py
+++ services/notification/tests/unit/test_comparison.py
@@ -1,5 +1,3 @@
-from shared.reports.types import Change
-
 from services.comparison import ComparisonProxy, FilteredComparison
 
 
@@ -14,18 +12,3 @@ class TestFilteredComparison(object):
         assert isinstance(filtered_comparison, FilteredComparison)
         res = filtered_comparison.get_existing_statuses()
         assert res == mocked_get_existing_statuses.return_value
-
-    def test_get_changes_rust_vs_python(self, mocker):
-        mocker.patch.object(ComparisonProxy, "get_diff")
-        mocker.patch(
-            "services.comparison.get_changes",
-            return_value=[Change(path="apple"), Change(path="pear")],
-        )
-        mocker.patch(
-            "services.comparison.get_changes_using_rust",
-            return_value=[Change(path="banana"), Change(path="pear")],
-        )
-        comparison = ComparisonProxy(mocker.MagicMock())
-        res = comparison.get_changes()
-        expected_result = [Change(path="apple"), Change(path="pear")]
-        assert expected_result == res
