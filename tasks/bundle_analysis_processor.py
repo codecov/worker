497150d3fc867fe68bc6eede5b31fce441d22614
Jerry Feng <jerry.feng@sentry.io>
2025-03-27 14:06:56 -0400

    Add a logging

--- tasks/bundle_analysis_processor.py
+++ tasks/bundle_analysis_processor.py
@@ -196,6 +196,17 @@ class BundleAnalysisProcessorTask(
 
             processing_results.append(result.as_dict())
         except (CeleryError, SoftTimeLimitExceeded, SQLAlchemyError):
+            log.exception(
+                "Unable to process bundle analysis upload",
+                extra=dict(
+                    repoid=repoid,
+                    commit=commitid,
+                    commit_yaml=commit_yaml,
+                    params=params,
+                    upload_id=upload.id_,
+                    parent_task=self.request.parent_id,
+                ),
+            )
             raise
         except Exception:
             log.exception(
