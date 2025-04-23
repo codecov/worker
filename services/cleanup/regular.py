378540a97c81e08e10be4a925cc8cff06f2d21fd
Arpad Borsos <arpad.borsos@sentry.io>
2025-04-23 12:12:47 +0200

    Add regular `Upload` cleanup
    This adds logic clean up `Upload`s older than 120 days currently.
    
    The logic to do this is carefully crafted to spread out the deletion into smaller chunks, as the production `Upload` table is one of the largest we have, and bulk-deletion of all uploads will probably cause other DB issues.

--- services/cleanup/regular.py
+++ services/cleanup/regular.py
@@ -1,5 +1,9 @@
 import logging
 import random
+from datetime import datetime, timedelta, timezone
+
+from django.db.models.query import QuerySet
+from shared.django_apps.reports.models import ReportSession as Upload
 
 from services.cleanup.cleanup import run_cleanup
 from services.cleanup.utils import CleanupResult, CleanupSummary, cleanup_context
@@ -11,8 +15,7 @@ def run_regular_cleanup() -> CleanupSummary:
     log.info("Starting regular cleanup job")
     complete_summary = CleanupSummary(CleanupResult(0), summary={})
 
-    # Usage of these model was removed, and we should clean up all its data before dropping the table for good.
-    cleanups_to_run = []
+    cleanups_to_run = create_upload_cleanup_jobs()
 
     # as we expect this job to have frequent retries, and cleanup to take a long time,
     # lets shuffle the various cleanups so that each one of those makes a little progress.
@@ -27,9 +30,51 @@ def run_regular_cleanup() -> CleanupSummary:
             complete_summary.add(summary)
 
     # TODO:
-    # - cleanup old `ReportSession`s (aka `Upload`s)
     # - cleanup `Commit`s that are `deleted`
     # - figure out a way how we can first mark, and then fully delete `Branch`es
 
     log.info("Regular cleanup finished")
     return complete_summary
+
+
+UPLOAD_RETENTION_PERIOD = 150
+MONTH_SLOTS = 120
+
+
+def create_upload_cleanup_jobs() -> list[QuerySet]:
+    """
+    This returns a list of `Upload` querysets, each targetting a subset of to-delete data.
+
+    As the `Upload` table is one of our biggest tables, running an (almost)
+    unbounded `DELETE` query would certainly cause problems.
+
+    Fortunately though, the (production) table has an index on `created_at`,
+    so queries targetting a range on that field should be fairly quick, and we
+    can use that to devide up the deletion workload onto more manageable chunks.
+
+    We are targetting 30-day chunks, going back ~10 years.
+    As the main cleanup task above is using a `random.shuffle`, and the cleanup
+    task itself is being restarted/retried on timeouts, this will end up with
+    an even distribution of cleanup tasks running concurrently, deleting different
+    chunks of this table.
+    """
+    latest_timestamp = datetime.now(timezone.utc) - timedelta(
+        days=UPLOAD_RETENTION_PERIOD
+    )
+    timestamps = [latest_timestamp]
+    for _ in range(MONTH_SLOTS):
+        timestamps.append(timestamps[-1] - timedelta(days=30))
+    timestamps.reverse()
+
+    begin_timestamp = None
+    queries: list[QuerySet] = []
+    for timestamp in timestamps:
+        query = Upload.objects
+        if begin_timestamp:
+            query = query.filter(created_at__gte=begin_timestamp)
+        query = query.filter(created_at__lt=timestamp)
+        queries.append(query)
+
+        begin_timestamp = timestamp
+
+    return queries
--- services/cleanup/tests/test_regular_cleanup.py
+++ services/cleanup/tests/test_regular_cleanup.py
@@ -1,11 +1,78 @@
-import pytest
+from datetime import datetime, timezone
 
-from services.cleanup.regular import run_regular_cleanup
+import pytest
+from freezegun import freeze_time
+from shared.bundle_analysis import StoragePaths
+from shared.django_apps.core.tests.factories import CommitFactory, RepositoryFactory
+from shared.django_apps.reports.models import ReportSession as Upload
+from shared.django_apps.reports.tests.factories import (
+    CommitReportFactory,
+    UploadFactory,
+)
+
+from services.archive import ArchiveService
+from services.cleanup.regular import create_upload_cleanup_jobs, run_regular_cleanup
+from services.cleanup.tests.test_relations import dump_delete_queries
 from services.cleanup.utils import CleanupResult, CleanupSummary
 
 
 @pytest.mark.django_db
-def test_runs_regular_cleanup():
+@freeze_time("2025-04-23T00:00:00Z")
+def test_runs_regular_cleanup(mock_storage):
+    repo = RepositoryFactory()
+    archive_service = ArchiveService(repo)
+
+    for i, timestamp in enumerate(
+        [
+            datetime(2024, 8, 1, tzinfo=timezone.utc),
+            datetime(2025, 1, 1, tzinfo=timezone.utc),
+        ]
+    ):
+        commit = CommitFactory(repository=repo)
+
+        commit_report = CommitReportFactory(commit=commit)
+        with freeze_time(timestamp):
+            upload = UploadFactory(report=commit_report, storage_path=f"upload{i}")
+
+        archive_service.write_chunks(commit.commitid, f"chunks_data{i}")
+        archive_service.write_file(upload.storage_path, f"upload_data{i}")
+
+        ba_report = CommitReportFactory(commit=commit, report_type="bundle_analysis")
+        with freeze_time(timestamp):
+            ba_upload = UploadFactory(report=ba_report, storage_path=f"ba_upload{i}")
+
+        ba_report_path = StoragePaths.bundle_report.path(
+            repo_key=archive_service.storage_hash, report_key=ba_report.external_id
+        )
+        archive_service.storage.write_file(
+            "bundle-analysis", ba_report_path, f"ba_report_data{i}"
+        )
+        archive_service.storage.write_file(
+            "bundle-analysis", ba_upload.storage_path, f"ba_upload_data{i}"
+        )
+
+    archive = mock_storage.storage["archive"]
+    ba_archive = mock_storage.storage["bundle-analysis"]
+    assert len(archive) == 4
+    assert len(ba_archive) == 4
+
     summary = run_regular_cleanup()
 
-    assert summary == CleanupSummary(CleanupResult(0, 0), {})
+    assert summary == CleanupSummary(
+        CleanupResult(2, 2),
+        {
+            Upload: CleanupResult(2, 2),
+        },
+    )
+    assert len(archive) == 3
+    assert len(ba_archive) == 3
+
+
+@pytest.mark.django_db
+@freeze_time("2025-04-23T00:00:00Z")
+def test_generates_sliced_upload_cleanups(snapshot):
+    # we expect the last slice to delete uploads older than `2024-11-24`,
+    # or 120 days older than the frozen timestamp given above.
+    latest = create_upload_cleanup_jobs()[-1]
+
+    assert dump_delete_queries(latest) == snapshot("upload.txt")
